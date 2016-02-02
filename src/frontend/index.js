3e80e3307c7afcdeda1646488f1784539120ce3c
dborisenkowork <dborisenkowork@gmail.com>
2016-02-03 00:49:11 +0300

    frontend / reset.css

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,6 +1,8 @@
 import angular from "angular";
 import app from './app/component';
 
+require("./node_modules/reset-css/reset.css");
+
 import WelcomeComponent from './app/component/welcome/WelcomeComponent/component';
 import SquareCenterComponent from './app/component/square/SquareCenterComponent/component';
 
