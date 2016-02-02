be397ac36e99610339426a8c6a38070bf07e9205
dborisenkowork <dborisenkowork@gmail.com>
2016-02-02 17:00:54 +0300

    frontend / destroy angular2. It's not production-ready.

--- src/frontend/app/component/square/home/SquareHomeComponent.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    template: require('./template.html')
-})
-export class SquareHomeComponent {
-}
--- src/frontend/app/component/welcome/WelcomeComponent.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core';
-import {RouteConfig, RouterOutlet} from 'angular2/router';
-
-@Component({
-    template: require('./template.html'),
-    directives: [RouterOutlet]
-})
-export class WelcomeComponent {}
