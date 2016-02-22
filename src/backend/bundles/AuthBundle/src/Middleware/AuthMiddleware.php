7cbcc88d2de8a15ed9ce9ee44881630e43b7efac
coffeeturbo <coffeeturbo@gmail.com>
2016-02-22 18:16:57 +0300

    Добавил Google провайдер

--- src/backend/bundles/AuthBundle/src/Middleware/AuthMiddleware.php
+++ src/backend/bundles/AuthBundle/src/Middleware/AuthMiddleware.php
@@ -9,6 +9,7 @@ use Auth\OauthProvider\Yandex;
 use Auth\Service\AuthService;
 use Auth\Service\AuthService\Exceptions\InvalidCredentialsException;
 use Doctrine\ORM\NoResultException;
+use League\OAuth2\Client\Provider\Google;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Zend\Stratigility\MiddlewareInterface;
@@ -64,7 +65,7 @@ class AuthMiddleware implements MiddlewareInterface
                             $this->oauthYandex($request, $responseBuilder);
                         break;
                         case 'google':
-                            $this->oauthVk($request, $responseBuilder);
+                            $this->oauthGoogle($request, $responseBuilder);
                         break;
                         case 'facebook':
                             $this->oauthVk($request, $responseBuilder);
@@ -259,6 +260,66 @@ class AuthMiddleware implements MiddlewareInterface
 
                 } catch(\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e){
                     exit($e->getMessage());
+                }
+            }
+        } catch(UnknownActionException $e){
+            $responseBuilder
+              ->setStatusBadRequest()
+              ->setError($e);
+        }
+    }
+    private function oauthGoogle(Request $request, GenericRESTResponseBuilder $responseBuilder){
+        try{
+            $provider = new Google(
+              [
+                'clientId'     => '992284499181-ccsf7gh4ucck8348g9kaimqs2s9i2fjt.apps.googleusercontent.com',
+                'clientSecret' => 'VLSX8IMfFfkAll-dAtiEkUiZ',
+                'redirectUri'  => 'http://localhost:8080/backend/api/auth/oauth/google',
+                'hostedDomain' => 'localhost:8080',
+              ]
+            );
+
+            if (!empty($_GET['error'])) {
+
+                // Got an error, probably user denied access
+                exit('Got error: ' . $_GET['error']);
+
+            } elseif (empty($_GET['code'])) {
+
+                // If we don't have an authorization code then get one
+                $authUrl = $provider->getAuthorizationUrl();
+                $_SESSION['oauth2state'] = $provider->getState();
+                header('Location: ' . $authUrl);
+                exit;
+
+            } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+
+                // State is invalid, possible CSRF attack in progress
+                unset($_SESSION['oauth2state']);
+                exit('Invalid state');
+
+            } else {
+
+                // Try to get an access token (using the authorization code grant)
+                $token = $provider->getAccessToken('authorization_code', [
+                  'code' => $_GET['code']
+                ]);
+
+                // Optional: Now you have a token you can look up a users profile data
+                try {
+
+                    // We got an access token, let's now get the owner details
+                    $ownerDetails = $provider->getResourceOwner($token);
+
+                    // Use these details to create a new profile
+                    $responseBuilder->setStatusSuccess()
+                                    ->setJson([$ownerDetails]);
+
+                } catch (\Exception $e) {
+
+                    // Failed to get user details
+                    exit('Something went wrong: ' . $e->getMessage());
+
                 }
             }
         } catch(UnknownActionException $e){
