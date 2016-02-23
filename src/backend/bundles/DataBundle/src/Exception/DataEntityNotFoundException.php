9d38319524ae27f12df3aa14c550464a385b3a01
dborisenkowork <dborisenkowork@gmail.com>
2016-02-23 14:26:48 +0300

    backend / theme / entity not found exception

--- src/backend/bundles/DataBundle/src/Exception/DataEntityNotFoundException.php
+++ src/backend/bundles/DataBundle/src/Exception/DataEntityNotFoundException.php
@@ -1,4 +1,10 @@
 <?php
 namespace Data\Exception;
 
+/**
+ * Don't use doctrine2's EntityNotFound exception in middleware or services
+ *
+ * You have been warned.
+ */
+
 class DataEntityNotFoundException extends \Exception {}
