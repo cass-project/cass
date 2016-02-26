3b27688a096d9cbb3a9fa39cc46e9466ccb1e1d5
Philip Mantrov <philip.mantrov@gmail.com>
2016-02-26 17:40:35 +0300

    frontend / uc0002 / add form controller

--- src/frontend/app/module/host-admin/component/ThemesAdminService/index.ts
+++ src/frontend/app/module/host-admin/component/ThemesAdminService/index.ts
@@ -1,17 +1,19 @@
 import {Injectable} from 'angular2/core';
 import {Http, RequestOptions} from 'angular2/http';
 import {Headers} from "angular2/http";
+import {ControlGroup} from "angular2/common";
+import {Control} from "angular2/common";
 
 
 @Injectable()
 export class ManageThemes {
     themes;
+    themeId;
     public http:Http;
     active: boolean = true;
-    show: boolean = false;
-   public clicked(){
-        this.show = !this.show;
-    }
+    showAdd: boolean = false;
+    showUpdate: boolean = false;
+
     constructor(public http:Http) {
         this.http = http;
     }
@@ -21,25 +23,38 @@ export class ManageThemes {
             .map(res => res.json())
             .subscribe(data => this.themes = data.entities);
     }
-      putTheme(value){
+
+    takeTheme(theme){
+        this.themeId = theme.id;
+    }
+
+    putThemeClick() {
+       this.showAdd = !this.showAdd;
+    }
+    putTheme(tittle, parrentId){
         var headers = new Headers();
         headers.append('Content-type', 'application/json');
         var options = new RequestOptions({
             headers: headers
         });
-          return this.http.put('/backend/api/protected/host-admin/theme-editor/entity/create', JSON.stringify({title: value}), options).subscribe(
+        if(parrentId == undefined) parrentId = 0;
+        return this.http.put('/backend/api/protected/host-admin/theme-editor/entity/create', JSON.stringify({title: tittle, parent_id: parrentId}), options).subscribe(
             data =>{console.log(data); this.loadThemes();},
             err => {console.log(err);}
         );
     }
 
-    updateTheme(id, value){
+    updateThemeClick(){
+        this.showUpdate = !this.showUpdate;
+    }
+    updateTheme(id, value, parrentId){
         var headers = new Headers();
         headers.append('Content-type', 'application/json');
         var options = new RequestOptions({
             headers: headers
         });
-        this.http.post('/backend/api/protected/host-admin/theme-editor/entity/update/' + id, JSON.stringify({title: value}), options).subscribe(
+        if(parrentId == undefined) parrentId = 0;
+        this.http.post('/backend/api/protected/host-admin/theme-editor/entity/update/' + id, JSON.stringify({title: value, parent_id: parrentId}), options).subscribe(
             data =>{ console.log(data); this.loadThemes();},
             err => {console.log(err);}
         );
@@ -55,5 +70,9 @@ export class ManageThemes {
             data =>{ console.log(data); this.loadThemes();},
             err => {console.log(err);}
         );
+    }
+
+    openDialog(type){
+
     }
 };
