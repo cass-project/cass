e9158d616d0a99d93b8f85299db3fd8764670ed3
Philip Mantrov <philip.mantrov@gmail.com>
2016-02-29 18:46:01 +0300

    frontend / uc0002 / piece of tree-view

--- src/frontend/app/module/host-admin/component/ThemesAdminService/index.ts
+++ src/frontend/app/module/host-admin/component/ThemesAdminService/index.ts
@@ -5,32 +5,64 @@ import {ControlGroup} from "angular2/common";
 import {Control} from "angular2/common";
 
 
+
 @Injectable()
 export class ManageThemes {
     themes;
-    themeId;
-    public http:Http;
+    dirictories = [];
+    parentThemes = [];
+    childThemes = [];
+    themeId: number;
     active: boolean = true;
     showAdd: boolean = false;
     showUpdate: boolean = false;
+    showChild: boolean = false;
+    crudShow: boolean = false;
+    checked: boolean = false;
+    public http:Http;
 
     constructor(public http:Http) {
         this.http = http;
+
     }
 
     public loadThemes() {
         return this.http.get('/backend/api/protected/host-admin/theme-editor/read/entities/')
             .map(res => res.json())
-            .subscribe(data => this.themes = data.entities);
+            .subscribe(data => {this.themes = data.entities;
+                for(var i = 0; i < this.themes.length; i++){
+                if(this.themes[i].parent_id){
+                    this.dirThemes(this.themes[i].id, this.themes[i].parent_id)
+                } else this.parentThemes.push(this.themes[i]);
+            }});
+
+    }
+
+    getIcon(){
+
+        if(this.showChild){
+            return '-';
+        }
+
+        return '+';
+    }
+
+    eraseArrays(){
+        this.parentThemes = [];
+        this.childThemes = [];
+        this.showChild = false;
+    }
+
+    dirThemes(id, parentId){
+
     }
 
     takeTheme(theme){
         this.themeId = theme.id;
+
     }
 
-    putThemeClick() {
-       this.showAdd = !this.showAdd;
-    }
+
     putTheme(tittle, parrentId){
         var headers = new Headers();
         headers.append('Content-type', 'application/json');
@@ -39,14 +71,11 @@ export class ManageThemes {
         });
         if(parrentId == undefined) parrentId = 0;
         return this.http.put('/backend/api/protected/host-admin/theme-editor/entity/create', JSON.stringify({title: tittle, parent_id: parrentId}), options).subscribe(
-            data =>{console.log(data); this.loadThemes();},
+            data =>{console.log(data); this.eraseArrays(); this.loadThemes();},
             err => {console.log(err);}
         );
     }
 
-    updateThemeClick(){
-        this.showUpdate = !this.showUpdate;
-    }
     updateTheme(id, value, parrentId){
         var headers = new Headers();
         headers.append('Content-type', 'application/json');
@@ -55,19 +84,20 @@ export class ManageThemes {
         });
         if(parrentId == undefined) parrentId = 0;
         this.http.post('/backend/api/protected/host-admin/theme-editor/entity/update/' + id, JSON.stringify({title: value, parent_id: parrentId}), options).subscribe(
-            data =>{ console.log(data); this.loadThemes();},
+            data =>{console.log(data); console.log(this.themes, this.parentThemes); this.eraseArrays(); this.loadThemes();},
             err => {console.log(err);}
         );
     }
 
     deleteTheme(id) {
+        this.crudShow = !this.crudShow;
         var headers = new Headers();
         headers.append('Content-type', 'application/json');
         var options = new RequestOptions({
             headers: headers
         });
         this.http.delete('/backend/api/protected/host-admin/theme-editor/entity/delete/' + id, options).subscribe(
-            data =>{ console.log(data); this.loadThemes();},
+            data =>{ console.log(data); this.eraseArrays(); this.loadThemes();},
             err => {console.log(err);}
         );
     }
