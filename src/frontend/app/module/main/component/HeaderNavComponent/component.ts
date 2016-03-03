8697e447e1e032695039039c11a49cc3c694300b
hck <art.hacker@ya.ru>
2016-03-03 14:21:04 +0300

    UC-0001 / Frontend / HeaderNavComponent / Add auth auth-controls

--- src/frontend/app/module/main/component/HeaderNavComponent/component.ts
+++ src/frontend/app/module/main/component/HeaderNavComponent/component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {AuthService, AuthServiceProvider} from './../../../auth/service/AuthService';
 
 @Component({
     selector: 'cass-header-bar',
@@ -9,8 +10,14 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
     ],
     directives: [
         ROUTER_DIRECTIVES
-    ]
+    ],
+    providers: [AuthServiceProvider]
 })
 export class HeaderNavComponent
 {
+    authService:AuthService;
+
+    constructor(authServiceProvider:AuthServiceProvider) {
+        this.authService = authServiceProvider.getInstance();
+    }
 }
