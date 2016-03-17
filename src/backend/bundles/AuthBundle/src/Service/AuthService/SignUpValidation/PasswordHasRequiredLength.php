e4a32a2d3ccafefcc524326f296739f960a97083
dborisenko <dborisenkowork@gmail.com>
2016-03-17 16:28:41 +0300

    uc0001 / password

--- src/backend/bundles/AuthBundle/src/Service/AuthService/SignUpValidation/PasswordHasRequiredLength.php
+++ src/backend/bundles/AuthBundle/src/Service/AuthService/SignUpValidation/PasswordHasRequiredLength.php
@@ -7,10 +7,10 @@ use Auth\Service\AuthService\Exceptions\ValidationException;
 class PasswordHasRequiredLength implements Validator
 {
     public function validate(array $request) {
-        $isValid = preg_match('~((?=.*[a-z])(?=.*\d)(?=.*[A-Z]).{6,})~', $request['password']) == 0;
+        $isValid = strlen($request['password']) < 6 || strlen($request['password']) > 40;
 
         if(!$isValid) {
-            throw new ValidationException('Password must be at least 6 characters with one uppercase letter and digit.');
+            throw new ValidationException('Password must be at least 6 characters');
         }
     }
 
