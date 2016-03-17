df2365f15c22e9636d6e4291600e5353bfb6a5d0
Philip Mantrov <philip.mantrov@gmail.com>
2016-03-17 13:50:42 +0300

    vagrant / fix \r\n

--- support-files/provision.sh
+++ support-files/provision.sh
@@ -1,6 +1,11 @@
 cp -R /support-files/copy/usr/bin/vm-init /usr/bin/
 cp -R /support-files/copy/usr/bin/vm-server-update /usr/bin/
 cp -R /support-files/copy/usr/bin/vm-project-update /usr/bin/
+
 chmod a+x /usr/bin/vm-server-update
 chmod a+x /usr/bin/vm-project-update
 chmod a+x /usr/bin/vm-init
+
+sudo sed -i 's/\r$//' /usr/bin/vm-init
+sudo sed -i 's/\r$//' /usr/bin/vm-project-update
+sudo sed -i 's/\r$//' /usr/bin/vm-server-update
