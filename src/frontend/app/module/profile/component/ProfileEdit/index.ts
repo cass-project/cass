1b740359f897bea7ccb27276ade864303f607b5d
Philip Mantrov <philip.mantrov@gmail.com>
2016-04-06 16:14:40 +0300

    uc0008 / frontend / add new routes
    /profile/personal
    /profile/edit
    /profile - cap

---
+++ src/frontend/app/module/profile/component/ProfileEdit/index.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    template: require('./template.html'),
+    directives: [],
+    styles: [
+        require('./style.shadow.scss')
+    ]
+})
+export class ProfileEdit
+{
+}
--- src/frontend/app/module/profile/index.ts
+++ src/frontend/app/module/profile/index.ts
@@ -3,6 +3,8 @@ import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {AuthService} from '../auth/service/AuthService';
 import {ProfileChannelComponent} from './component/ProfileChannelComponent/index';
 import {ProfilePersonalDataComponent} from './component/ProfilePersonalDataComponent/index';
+import {ThemeCleaner} from "../host-admin/component/ThemeCleaner/component";
+import {ProfileEdit} from "./component/ProfileEdit/index";
 
 @Component({
     template: require('./template.html'),
@@ -15,15 +17,25 @@ import {ProfilePersonalDataComponent} from './component/ProfilePersonalDataCompo
 })
 @RouteConfig([
     {
+        useAsDefault: true,
+        path: '/',
+        name: 'Theme-Cleaner',
+        component: ThemeCleaner
+    },
+    {
         path: '/personal',
         name: 'ProfilePersonalData',
-        component: ProfilePersonalDataComponent,
-        useAsDefault: true
+        component: ProfilePersonalDataComponent
     },
     {
         path: '/channel/:channelId',
         name: 'ProfileChannel',
         component: ProfileChannelComponent
+    },
+    {
+        path: '/edit',
+        name: 'ProfileEdit',
+        component: ProfileEdit
     }
 ])
 export class ProfileComponent
