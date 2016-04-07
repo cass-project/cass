2d655af077f35dc39205d4fb0bcdd501aa6ff715
dborisenko <dborisenkowork@gmail.com>
2016-04-07 16:12:06 +0300

    master / profile bundle

---
+++ src/backend/bundles/Profile/src/ProfileBundle.php
@@ -0,0 +1,12 @@
+<?php
+namespace Profile;
+
+use Application\Bootstrap\Bundle\GenericBundle;
+
+class ProfileBundle extends GenericBundle
+{
+    public function getDir()
+    {
+        return __DIR__;
+    }
+}
