81c850aa5302716310c9908c4533de7793ef67e8
PhilipMantrov <philip.mantrov@gmail.com>
2016-04-18 17:21:50 +0300

    profile / frontend / rework AccountWelcome

--- src/frontend/app/module/profile/component/AccountWelcome/component.ts
+++ src/frontend/app/module/profile/component/AccountWelcome/component.ts
@@ -1,22 +1,22 @@
 import {Component, Injectable} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES, Router} from 'angular2/router';
-import {CurrentProfileRestService} from "../../service/CurrentProfileRestService";
-import {AvatarCropper} from "./AvatarCropper/index";
+import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {ProfileService} from "../../service/ProfileService";
+import {AvatarCropper} from "../AvatarCropper/index";
 import {CORE_DIRECTIVES} from "angular2/common";
-import {AccountWelcomeService} from "./service";
 import {AuthService} from "../../../auth/service/AuthService";
-import {ProfileWelcomeInfo} from "../../service/CurrentProfileRestService";
+import {ProfileWelcomeInfo} from "../../service/ProfileService";
+import {Profile} from "../../entity/Profile";
+import {AvatarCropperService} from "../AvatarCropper/service";
+import {AccountConfirm} from "./Confirm/component";
+import {AccountWelcomeHome} from "./AccountWelcomeHome/component";
 
 declare var Cropper;
 
 @Component({
     template: require('./template.html'),
-    styles: [
-        require('./style.shadow.scss'),
-    ],
     'providers': [
-        CurrentProfileRestService,
-        AccountWelcomeService,
+        ProfileService,
+        AvatarCropperService,
         AuthService
     ],
     directives: [
@@ -26,43 +26,21 @@ declare var Cropper;
     ]
 })
 
+@RouteConfig([
+    {
+        useAsDefault: true,
+        name: 'Home',
+        path: '/',
+        component: AccountWelcomeHome
+    },
+    {
+        name: 'Confirm',
+        path: '/confirm',
+        component: AccountConfirm
+    }
+])
+
 @Injectable()
 export class AccountWelcome {
-    constructor (private currentProfileRestService: CurrentProfileRestService,
-                 public router: Router,
-                 public accountWelcomeService: AccountWelcomeService,
-                 public authService: AuthService
-    ){}
-
-
-    profileInfo: ProfileWelcomeInfo = new ProfileWelcomeInfo();
-
-    preInfo: boolean = false;
-
-    chooseType: boolean = true;
-    chooseFL: boolean = false;
-    chooseLFM: boolean = false;
-    chooseN: boolean = false;
-
-    NextShow: boolean = false;
-    SubmitShow: boolean = false;
-    PreInfo: boolean = false;
-
-    showAvatarCropper() {
-        this.accountWelcomeService.isAvatarFormVisibleFlag = true;
-    }
-
-    isAvatarFormVisible() {
-        return this.accountWelcomeService.isAvatarFormVisibleFlag;
-    }
-
-    reset(){
-        this.router.parent.navigate(['Profile']);
-        this.router.parent.navigate(['Welcome']);
-    }
-
-    ngSubmit(){
-        this.router.navigate(['Profile']);
-    }
 }
 
