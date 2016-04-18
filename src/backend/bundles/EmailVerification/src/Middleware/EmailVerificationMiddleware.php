83c1af779e4d9ea3561e52723e6848fd8b36ae59
hck <art.hacker@ya.ru>
2016-04-18 16:31:07 +0300

    emailVarification / backend / middleware / init

--- src/backend/bundles/EmailVerification/src/Middleware/EmailVerificationMiddleware.php
+++ src/backend/bundles/EmailVerification/src/Middleware/EmailVerificationMiddleware.php
@@ -1,14 +1,48 @@
 <?php
 namespace EmailVerification\Middleware;
 
+use Common\REST\Exceptions\UnknownActionException;
+use Common\REST\GenericRESTResponseBuilder;
+use EmailVerification\Middleware\Command\Command;
+use EmailVerification\Service\EmailVerificationService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Zend\Stratigility\MiddlewareInterface;
 
 class EmailVerificationMiddleware implements MiddlewareInterface
 {
+    private $emailVerificationService;
+
+    public function __construct(EmailVerificationService $emailVerificationService)
+    {
+        $this->emailVerificationService = $emailVerificationService;
+    }
+
     public function __invoke(Request $request, Response $response, callable $out = null)
     {
-        return [];
+        $responseBuilder = new GenericRESTResponseBuilder($response);
+
+        try {
+            $command = Command::factory($request, $this->emailVerificationService);
+            $result = $command->run($request);
+
+            if ($result === true) {
+                $result = [];
+            }
+
+            $responseBuilder
+                ->setStatusSuccess()
+                ->setJson($result);
+
+        } catch (\Exception $e) {
+            $responseBuilder
+                ->setStatusBadRequest()
+                ->setError($e);
+        }
+
+
+
+        return $responseBuilder->build();
     }
+
 }
