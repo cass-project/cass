55bca1d82030f6cb7ce3fdc8a423af775aa0d679
dborisenko <dborisenkowork@gmail.com>
2016-04-21 14:40:43 +0300

    theme / create

--- src/backend/bundles/Theme/src/Service/ThemeService.php
+++ src/backend/bundles/Theme/src/Service/ThemeService.php
@@ -2,6 +2,7 @@
 namespace Theme\Service;
 
 use Auth\Service\CurrentAccountService;
+use Theme\Entity\Theme;
 use Theme\Repository\ThemeRepository;
 
 class ThemeService
@@ -12,8 +13,14 @@ class ThemeService
     /** @var ThemeRepository */
     private $themeRepository;
 
-    public function __construct(CurrentAccountService $currentAccountService, ThemeRepository $themeRepository) {
+    public function __construct(CurrentAccountService $currentAccountService, ThemeRepository $themeRepository)
+    {
         $this->currentAccountService = $currentAccountService;
         $this->themeRepository = $themeRepository;
+    }
+
+    public function createTheme(string $title, string $description, int $parentId = null): Theme
+    {
+        return $this->themeRepository->createTheme($title, $description, $parentId);
     }
 }
