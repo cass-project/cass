d4ab427b9c6bb7c2e0ca8d908215288578f15111
PhilipMantrov <philip.mantrov@gmail.com>
2016-04-28 14:36:01 +0300

    main_menu / frontend / fix return actual name

--- src/frontend/app/module/common/component/MainMenu/items/profile/index.ts
+++ src/frontend/app/module/common/component/MainMenu/items/profile/index.ts
@@ -2,6 +2,7 @@ import {Component, ViewEncapsulation} from "angular2/core";
 import {AuthService} from '../../../../../auth/service/AuthService';
 import {Profile} from "../../../../../profile/entity/Profile";
 import {ROUTER_DIRECTIVES} from "angular2/router";
+import {ProfileInfo} from "../../../../../profile/service/ProfileService";
 
 @Component({ // 1
     selector: "main-menu-item-profile",
@@ -15,7 +16,23 @@ import {ROUTER_DIRECTIVES} from "angular2/router";
 })
 export class MainMenuProfileItem
 {
-    public getTitle(): string {
+
+    getProfileName(){
+        if(this.getGreetings()){
+            switch(AuthService.getAuthToken().getCurrentProfile().entity.greetings.greetings_method){
+                case 'fl': return `${AuthService.getAuthToken().getCurrentProfile().entity.greetings.first_name} ${AuthService.getAuthToken().getCurrentProfile().entity.greetings.last_name}`;
+                    break;
+                case 'flm': return `${AuthService.getAuthToken().getCurrentProfile().entity.greetings.first_name} ${AuthService.getAuthToken().getCurrentProfile().entity.greetings.last_name} ${AuthService.getAuthToken().getCurrentProfile().entity.greetings.middle_name}`;
+                    break;
+                case 'n': return `${AuthService.getAuthToken().getCurrentProfile().entity.greetings.nickname}`;
+                    break;
+            }
+        } else return this.getGreetings();
+
+    }
+
+
+    public getGreetings() {
         return AuthService.isSignedIn()
             ? AuthService.getAuthToken().getCurrentProfile().greetings()
             : 'Anonymous';
