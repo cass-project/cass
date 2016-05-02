bd8296dbac0034331582993958f5c477035a9c2a
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-02 14:46:02 +0300

    avatar-cropper / frontend / add progressBar

--- src/frontend/app/module/profile/service/ProfileService.ts
+++ src/frontend/app/module/profile/service/ProfileService.ts
@@ -9,16 +9,17 @@ import {Headers} from "angular2/http";
 import {RequestOptions} from "angular2/http";
 import {AvatarCropperService} from "../component/AvatarCropper/service";
 import {AuthService} from "../../auth/service/AuthService";
+import {ThemeSelector} from "../../theme/component/ThemeSelector/component";
+import {ThemeService} from "../../theme/service/ThemeService";
 
 @Injectable()
 export class ProfileService {
     constructor(public http:Http,
-                public avatarCropperService:AvatarCropperService
+                public avatarCropperService:AvatarCropperService,
+                public themeService: ThemeService
     ){}
 
-
-    progress = '0';
-
+    public progressBar: number;
     public profileInfo: ProfileInfo = new ProfileInfo();
 
     getProfileInfo() {
@@ -61,26 +62,58 @@ export class ProfileService {
         }));
     }
 
-    progressFunction(evt) {
-        if (evt.lengthComputable) {
-            this.progress = Math.round(evt.loaded / evt.total * 100) + "%";
+    getSelectedThemesId(){
+        let arrayPush = [];
+
+        for(let i = 0; i < this.themeService.selectedThemes.length; i++){
+            arrayPush.push(this.themeService.selectedThemes[i].id)
         }
+
+        return arrayPush;
     }
 
-        avatarUpload(file:Blob, crop:Crop) {
+    createExpertList(){
+        let url = `backend/api/protected/profile/${AuthService.getAuthToken().getCurrentProfile().entity.id}/expert-in`;
+
+
+        return this.http.put(url, JSON.stringify({
+            theme_id: this.getSelectedThemesId()
+        }));
+    }
+
+
+    createInterestList(){
+        let url = `backend/api/protected/profile/${AuthService.getAuthToken().getCurrentProfile().entity.id}/interesting-in`;
+
+        return this.http.put(url, JSON.stringify({
+            theme_id: this.themeService.selectedThemes
+        }));
+    }
+
+
+
+
+
+    avatarUpload(file:Blob, crop:Crop) {
         let url = `/backend/api/protected/profile/${AuthService.getAuthToken().getCurrentProfile().entity.id}/image-upload/crop-start/${crop.start.x}/${crop.start.y}/crop-end/${crop.end.x}/${crop.end.y}`;
         let formData = new FormData();
         formData.append("file", file);
 
         xmlRequest.open("POST", url);
+        xmlRequest.upload.onprogress = (e) => {
+            if (e.lengthComputable) {
+               this.progressBar  = Math.floor((e.loaded / e.total) * 100);
+            }
+        };
+
         xmlRequest.send(formData);
-        xmlRequest.upload.addEventListener("progress", this.progressFunction, false);
 
         xmlRequest.onreadystatechange = () => {
             if (xmlRequest.readyState === 4) {
                 if (xmlRequest.status === 200) {
                     this.avatarCropperService.isAvatarFormVisibleFlag = false;
                     AuthService.getAuthToken().getCurrentProfile().entity.image.public_path = JSON.parse(xmlRequest.responseText).public_path;
+                    this.progressBar = 0;
                 }
             }
         }
