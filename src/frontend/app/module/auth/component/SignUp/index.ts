19d8e5448edb5ec043d4b132d7b301583d109c02
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-12 15:14:39 +0300

    auth / frontend / SignUp

--- src/frontend/app/module/auth/component/SignUp/index.ts
+++ src/frontend/app/module/auth/component/SignUp/index.ts
@@ -1,6 +1,9 @@
 import {Component} from "angular2/core";
+import {Router} from 'angular2/router';
 import {LoadingIndicator} from "../../../util/component/LoadingIndicator/index";
 import {OAuth2Component} from "../OAuth2/index";
+import {AuthComponentService} from "../Auth/service";
+import {AuthService} from "../../service/AuthService";
 
 @Component({
     selector: 'cass-auth-sign-up',
@@ -15,4 +18,39 @@ import {OAuth2Component} from "../OAuth2/index";
 })
 export class SignUpComponent
 {
+    private loading = false;
+    private personalInfo = {
+        email: '',
+        password: '',
+        repeat: '',
+        remember: false
+    };
+
+    constructor(private authService: AuthService, private service: AuthComponentService, private router: Router){}
+
+    attemptSignUp() {
+        this.loading = true;
+        this.personalInfo.remember = true;
+
+        this.authService.attemptSignUp(this.personalInfo).add(() => {
+            if(!this.authService.lastError) {
+                this.router.navigate(['/']);
+            }
+
+            this.loading = false;
+        });
+    }
+
+    attemptSignUpNoRemember() {
+        this.loading = true;
+        this.personalInfo.remember = false;
+
+        this.authService.attemptSignUp(this.personalInfo).add(() => {
+            if(!this.authService.lastError) {
+                this.router.navigate(['/']);
+            }
+
+            this.loading = false;
+        });
+    }
 }
