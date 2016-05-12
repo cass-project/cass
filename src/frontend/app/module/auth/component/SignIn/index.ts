123a74ab138bc62261f8d920d664bd57e109a6a0
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-12 17:58:31 +0300

    frontend / auth / new stage SignIn

--- src/frontend/app/module/auth/component/SignIn/index.ts
+++ src/frontend/app/module/auth/component/SignIn/index.ts
@@ -26,34 +26,19 @@ export class SignInComponent
     private loading = false;
     private personalInfo = {
         email: "",
-        password: "",
-        remember: false,
+        password: ""
     };
 
     constructor(private authService: AuthService, private service: AuthComponentService, private router: Router){}
 
     attemptSignIn(){
         this.loading = true;
-        this.personalInfo.remember = true;
 
         this.authService.attemptSignIn(this.personalInfo).add(() => {
             this.loading = false;
 
             if(!this.authService.lastError) {
-                this.router.navigate(['/']);
-            }
-        });
-    }
-
-    attemptSignInNoRemember() {
-        this.loading = true;
-        this.personalInfo.remember = false;
-
-        this.authService.attemptSignIn(this.personalInfo).add(() => {
-            this.loading = false;
-
-            if(!this.authService.lastError) {
-                this.router.navigate(['/']);
+                this.service.modals.closeModals();
             }
         });
     }
