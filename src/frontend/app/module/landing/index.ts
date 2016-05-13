3e43720a794acc891f00aee79e29ed3d31b08806
dborisenko <dborisenkowork@gmail.com>
2016-05-13 19:05:29 +0300

    frontend / theme / theme-select landing

--- src/frontend/app/module/landing/index.ts
+++ src/frontend/app/module/landing/index.ts
@@ -1,12 +1,14 @@
 import {Component} from "angular2/core";
 import {AuthComponent} from "../auth/component/Auth/index";
 import {ProfileSetup} from "../profile/component/ProfileSetup/index";
+import {ThemeSelect} from "../theme/component/ThemeSelect/index";
 
 @Component({
     template: require('./template.html'),
     directives: [
         AuthComponent,
         ProfileSetup,
+        ThemeSelect
     ]
 })
 export class LandingComponent {
