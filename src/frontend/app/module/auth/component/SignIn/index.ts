9a0eb3425d4d350b9bfd6d63907848fb91b4b351
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-13 17:48:41 +0300

    frontend / auth / SignIn
    Final version

--- src/frontend/app/module/auth/component/SignIn/index.ts
+++ src/frontend/app/module/auth/component/SignIn/index.ts
@@ -11,7 +11,7 @@ import {AuthService} from "../../service/AuthService";
         template-stages содержит всю разработанную для компонента верстку
         template же должен быть рабочим вариантом
      */
-    template: require('./template-stages.html'),
+    template: require('./template.html'),
     selector: 'cass-auth-sign-in',
     styles: [
         require('./style.shadow.scss')
@@ -28,6 +28,11 @@ export class SignInComponent
         email: "",
         password: ""
     };
+
+    checkEmail(){
+        let regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regexp.test(this.personalInfo.email);
+    }
 
     constructor(private authService: AuthService, private service: AuthComponentService, private router: Router){}
 
