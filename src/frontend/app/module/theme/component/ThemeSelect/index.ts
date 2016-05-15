8d4867c3d0fb26619452bbb86573ac5cd3b77ad4
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-16 00:09:52 +0300

    frontend / theme / ThemeSelector
    Added getThemeListAll() in ThemeService.
    Finish search feature.

--- src/frontend/app/module/theme/component/ThemeSelect/index.ts
+++ src/frontend/app/module/theme/component/ThemeSelect/index.ts
@@ -1,17 +1,28 @@
 import {Component, EventEmitter,Output} from "angular2/core";
+import {ThemeService} from "../../service/ThemeService";
 
 @Component({
     selector: 'cass-theme-select',
     template: require('./template.html'),
+    'providers': [
+        ThemeService
+    ],
     styles: [
         require('./style.shadow.scss')
     ]
 })
 export class ThemeSelect
 {
+    constructor(private themeService: ThemeService){}
+
+    themes;
     searchStr: string = '';
     tmpSearchStr: string = '';
     listVisibleByClick: boolean = false;
+
+    ngOnInit(){
+        this.getAllThemes();
+    }
 
     showListByClick(){
         this.listVisibleByClick = !this.listVisibleByClick;
@@ -28,6 +39,14 @@ export class ThemeSelect
         }
     }
 
+
+    getAllThemes(){
+        this.themeService.getThemeListAll().subscribe(data => {
+            this.themes = data;
+            this.themes = JSON.parse(this.themes._body).entities;
+        });
+    }
+
     /*ToDo: Когда Theme API будет добавлено сделать нормальное отображение активного селекта через [class.active]*/
     checkActiveTheme(themeId){
         return themeId
@@ -41,6 +60,11 @@ export class ThemeSelect
     search(){
         let results = [];
 
+        for(let i = 0; i < this.themes.length; i++){
+            if(this.themes[i].title.toLowerCase().indexOf(this.searchStr.toLowerCase())!=-1){
+                results.push(this.themes[i])
+            }
+        }
 
         return results
     }
--- src/frontend/app/module/theme/service/ThemeService.ts
+++ src/frontend/app/module/theme/service/ThemeService.ts
@@ -1,3 +1,16 @@
-/**
- * Created by NiiNii on 5/15/2016.
- */
+import {Injectable} from 'angular2/core';
+import {Http, URLSearchParams} from 'angular2/http';
+import {FrontlineService} from "../../frontline/service";
+
+@Injectable()
+export class ThemeService
+{
+    constructor(public http: Http){}
+
+    getThemeListAll(){
+        let url = 'backend/api/theme/get/list-all';
+
+        return this.http.get(url);
+    }
+
+}
