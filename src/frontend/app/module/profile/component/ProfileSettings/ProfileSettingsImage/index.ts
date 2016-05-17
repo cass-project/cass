70ec6186de57f3271244985cf6f6d4a6c7699d2c
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-17 22:37:22 +0300

    frontend / profile / ProfileSettingsImage
    Test step for AvatarCropper

--- src/frontend/app/module/profile/component/ProfileSettings/ProfileSettingsImage/index.ts
+++ src/frontend/app/module/profile/component/ProfileSettings/ProfileSettingsImage/index.ts
@@ -1,12 +1,25 @@
 import {Component} from "angular2/core";
+import {AvatarCropper} from "../../../../util/component/AvatarCropper/index";
+import {AvatarCropperService} from "../../../../util/component/AvatarCropper/service";
 
 @Component({
     selector: 'cass-profile-settings-image',
     template: require('./template.html'),
     styles: [
         require('./style.shadow.scss')
+    ],
+    directives: [
+        AvatarCropper
     ]
 })
 export class ProfileSettingsImage
 {
+    constructor(private avatarCropperService: AvatarCropperService){}
+
+
+
+    showAvatarCropper() {
+        this.avatarCropperService.isAvatarFormVisibleFlag = true;
+    }
+
 }
