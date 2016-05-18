f2491d05322bc75a6a1126c7ad4cdb5d7df857e5
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-19 00:52:09 +0300

    frontend / profile / Model + new template

--- src/frontend/app/module/profile/component/ProfileModal/model.ts
+++ src/frontend/app/module/profile/component/ProfileModal/model.ts
@@ -1,8 +1,31 @@
 import {Injectable} from "angular2/core";
+import {ProfileRESTService} from "../ProfileService/ProfileRESTService";
+import {ProfileService} from "../ProfileService/ProfileService";
+
 
 @Injectable()
 export class ProfileModalModel
 {
+    constructor(private profileRESTService: ProfileRESTService, private profileService: ProfileService) {}
+
+    needToSave: boolean = false;
+
+    onChange(event){
+        if(event){
+            this.needToSave = true;
+            console.log(this.needToSave);
+        }
+    }
+
+    saveAllChanges(){
+
+        /*Accont section*/
+        if(this.profileRESTService.changePasswordStn.new_password === this.profileRESTService.changePasswordStn.repeat_password &&
+            this.profileRESTService.changePasswordStn.new_password.length > 3){
+            this.profileRESTService.changePassword();
+        }
+    }
+
     hasChanges(): boolean {
         return false;
     }
