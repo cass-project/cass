82f6f215972f9e3b5c85732369a369aec9ff48e4
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-18 04:57:14 +0300

    frontend / profile / ProfileSettingsImage
    Dunno what this component doing, lol.
    But anyway save my changes.

--- src/frontend/app/module/profile/component/ProfileSettings/ProfileSettingsImage/index.ts
+++ src/frontend/app/module/profile/component/ProfileSettings/ProfileSettingsImage/index.ts
@@ -1,4 +1,6 @@
-import {Component} from "angular2/core";
+import {Component, Injectable} from "angular2/core";
+import {CORE_DIRECTIVES} from "angular2/common";
+import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {AvatarCropper} from "../../../../util/component/AvatarCropper/index";
 import {AvatarCropperService} from "../../../../util/component/AvatarCropper/service";
 
@@ -8,18 +10,33 @@ import {AvatarCropperService} from "../../../../util/component/AvatarCropper/ser
     styles: [
         require('./style.shadow.scss')
     ],
+    providers: [
+        AvatarCropperService
+    ],
     directives: [
-        AvatarCropper
+        ROUTER_DIRECTIVES,
+        CORE_DIRECTIVES,
+        /*AvatarCropper*/
     ]
 })
+
+@Injectable()
 export class ProfileSettingsImage
 {
     constructor(private avatarCropperService: AvatarCropperService){}
 
 
+    isAvatarCropperVisible(){
+        return this.avatarCropperService.isAvatarFormVisibleFlag;
+    }
 
     showAvatarCropper() {
         this.avatarCropperService.isAvatarFormVisibleFlag = true;
     }
 
+    private onFileChange(event) : void {
+        this.avatarCropperService.file = event.target.files[0];
+        this.avatarCropperService.imgPath = URL.createObjectURL(event.target.files[0]);
+        this.showAvatarCropper();
+    }
 }
