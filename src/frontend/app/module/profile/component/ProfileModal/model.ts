9519c64857320c60e92b66dcd3bff45df8dacb7f
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-19 04:06:31 +0300

    frontend / profile / model

--- src/frontend/app/module/profile/component/ProfileModal/model.ts
+++ src/frontend/app/module/profile/component/ProfileModal/model.ts
@@ -1,19 +1,33 @@
 import {Injectable} from "angular2/core";
 import {ProfileRESTService} from "../ProfileService/ProfileRESTService";
 import {ProfileService} from "../ProfileService/ProfileService";
+import {ProfileComponentService} from "../../service";
 
 
 @Injectable()
 export class ProfileModalModel
 {
-    constructor(private profileRESTService: ProfileRESTService, private profileService: ProfileService) {}
+    constructor(private profileRESTService: ProfileRESTService, private profileService: ProfileService, private modals: ProfileComponentService) {}
 
     needToSave: boolean = false;
 
+    cancel(){
+        this.profileRESTService.changePasswordStn.old_password = '';
+        this.profileRESTService.changePasswordStn.new_password = '';
+        this.profileRESTService.changePasswordStn.repeat_password = '';
+
+        this.needToSave = false;
+    }
+
+    canSave(){
+        if(this.needToSave && this.profileRESTService.changePasswordStn.new_password === this.profileRESTService.changePasswordStn.repeat_password){
+            return true;
+        }
+    }
+
     onChange(event){
-        if(event){
+        if(event) {
             this.needToSave = true;
-            console.log(this.needToSave);
         }
     }
 
@@ -21,8 +35,8 @@ export class ProfileModalModel
 
         /*Accont section*/
         if(this.profileRESTService.changePasswordStn.new_password === this.profileRESTService.changePasswordStn.repeat_password &&
-            this.profileRESTService.changePasswordStn.new_password.length > 3){
-            this.profileRESTService.changePassword();
+            this.profileRESTService.changePasswordStn.new_password.length >= 3){
+            this.profileRESTService.changePassword().subscribe(data => {this.modals.modals.settings.close()});
         }
     }
 
