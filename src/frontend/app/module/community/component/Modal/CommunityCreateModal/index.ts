9826a0dd41a606b691e258a1e6ff034f332af80f
dborisenko <dborisenkowork@gmail.com>
2016-05-19 16:53:36 +0300

    frontend / community / create modal / screens

--- src/frontend/app/module/community/component/Modal/CommunityCreateModal/index.ts
+++ src/frontend/app/module/community/component/Modal/CommunityCreateModal/index.ts
@@ -4,6 +4,15 @@ import {EventEmitter} from "angular2/core";
 import {CommunityRESTService} from "../../../service/CommunityRESTService";
 import {ModalComponent} from "../../../../modal/component/index";
 
+enum CreateStage {
+    General = <any>"General",
+    Theme = <any>"Theme",
+    Image = <any>"Image",
+    Features = <any>"Features",
+    Processing = <any>"Processing",
+    Complete = <any>"Complete"
+}
+
 @Component({
     selector: 'cass-community-create-modal',
     template: require('./template.html'),
