28acd93ae1c28659a717cafb0aa68f7b04972113
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-22 18:50:17 +0300

    frontend / profile / ProfileRESTService

--- src/frontend/app/module/profile/component/ProfileService/ProfileRESTService.ts
+++ src/frontend/app/module/profile/component/ProfileService/ProfileRESTService.ts
@@ -7,95 +7,57 @@ import {AvatarCropperService} from "../../../util/component/AvatarCropper/servic
 import {ProfileModalModel} from "../ProfileModal/model";
 import {FrontlineService} from "../../../frontline/service";
 import {ThemeService} from "../../../theme/service/ThemeService";
+import {ProfileService} from "./ProfileService";
 
 @Injectable()
 export class ProfileRESTService
 {
-    constructor(public http: Http, private avatarCropperService: AvatarCropperService, private frontlineService: FrontlineService, private themeService: ThemeService){}
+    constructor(public http: Http, private avatarCropperService: AvatarCropperService,
+                private frontlineService: FrontlineService,
+                private themeService: ThemeService,
+                private profileService: ProfileService){}
 
     public tryNumber: number = 0;
     public progressBar: number;
-    public changePasswordStn = {old_password: '', new_password: '', repeat_password: ''};
-    greetings = {
-        id: 0,
-        profile_id: 0,
-        first_name: '',
-        last_name: '',
-        middle_name: '',
-        nickname: '',
-        greetings: '',
-        greetings_method: ''
-    };
-
-
-    personalCondReset(){
-        for (let key in this.frontlineService.session.auth.profiles[0].greetings) {
-            this.greetings[key] = this.frontlineService.session.auth.profiles[0].greetings[key];
-        }
-    }
-
-    personalCondToSave() {
-        if (this.greetings.id === 0) {
-            this.personalCondReset()
-        } else if (this.greetings.greetings_method != this.frontlineService.session.auth.profiles[0].greetings.greetings_method ||
-                   this.greetings.first_name != this.frontlineService.session.auth.profiles[0].greetings.first_name ||
-                   this.greetings.last_name != this.frontlineService.session.auth.profiles[0].greetings.last_name ||
-                   this.greetings.middle_name != this.frontlineService.session.auth.profiles[0].greetings.middle_name ||
-                   this.greetings.nickname != this.frontlineService.session.auth.profiles[0].greetings.nickname) {
-            return true;
-        }
-    }
-
-    accountCondReset(){
-        this.changePasswordStn.old_password = '';
-        this.changePasswordStn.new_password = '';
-        this.changePasswordStn.repeat_password = '';
-    }
-
-    accountCondToSave(){
-        if(this.changePasswordStn.new_password.length > 0 &&
-            this.changePasswordStn.repeat_password.length > 0 &&
-            this.changePasswordStn.new_password === this.changePasswordStn.repeat_password &&
-            this.changePasswordStn.new_password !== this.changePasswordStn.old_password){
-            return true;
-        } else if(this.changePasswordStn.new_password.length > 0 &&
-            this.changePasswordStn.repeat_password.length > 0 &&
-            this.changePasswordStn.new_password === this.changePasswordStn.old_password &&
-            this.changePasswordStn.repeat_password === this.changePasswordStn.old_password){
-            console.log("Старый и новые пароли совпадают");
-        }
-    }
-
-    /*interestCondReset(){
-        this.themeService.pickedInterestingInThemes = this.frontlineService.session.auth.profiles[0].interesting_in;
-        this.themeService.pickedExpertInThemes = this.frontlineService.session.auth.profiles[0].expert_in;
-    }
-
-    interestCondToSave(){
-        console.log(this.frontlineService.session.auth.profiles[0].interesting_in, this.themeService.pickedInterestingInThemes);
-        if(this.frontlineService.session.auth.profiles[0].interesting_in != this.themeService.pickedInterestingInThemes ||
-            this.frontlineService.session.auth.profiles[0].expert_in != this.themeService.pickedExpertInThemes){
-            return true;
-        }
-    }*/
 
     editPersonal(){
         let url = `/backend/api/protected/profile/${this.frontlineService.session.auth.profiles[0].id}/edit-personal/`;
 
-        this.frontlineService.session.auth.profiles[0].greetings.greetings_method = this.greetings.greetings_method;
-        this.frontlineService.session.auth.profiles[0].greetings.last_name =  this.greetings.last_name;
-        this.frontlineService.session.auth.profiles[0].greetings.first_name =  this.greetings.first_name;
-        this.frontlineService.session.auth.profiles[0].greetings.middle_name =  this.greetings.middle_name;
-        this.frontlineService.session.auth.profiles[0].greetings.nickname =  this.greetings.nickname;
+        this.frontlineService.session.auth.profiles[0].greetings.greetings_method = this.profileService.greetings.greetings_method;
+        this.frontlineService.session.auth.profiles[0].greetings.last_name =  this.profileService.greetings.last_name;
+        this.frontlineService.session.auth.profiles[0].greetings.first_name =  this.profileService.greetings.first_name;
+        this.frontlineService.session.auth.profiles[0].greetings.middle_name =  this.profileService.greetings.middle_name;
+        this.frontlineService.session.auth.profiles[0].greetings.nickname =  this.profileService.greetings.nickname;
 
         return this.http.post(url, JSON.stringify({
-            greetings_method: this.greetings.greetings_method,
-            last_name: this.greetings.last_name,
-            first_name: this.greetings.first_name,
-            middle_name: this.greetings.middle_name,
-            nickname: this.greetings.nickname
+            greetings_method: this.profileService.greetings.greetings_method,
+            last_name: this.profileService.greetings.last_name,
+            first_name: this.profileService.greetings.first_name,
+            middle_name: this.profileService.greetings.middle_name,
+            nickname: this.profileService.greetings.nickname
         }));
     }
+
+    updateExpertThemes(){
+        let url = `/backend/api/protected/profile/${AuthService.getAuthToken().getCurrentProfile().entity.id}/expert-in`;
+
+        this.frontlineService.session.auth.profiles[0].expert_in = (JSON.parse(JSON.stringify(this.profileService.expertIn)));
+
+        return this.http.put(url, JSON.stringify({
+            theme_ids: this.profileService.expertIn
+        }));
+    }
+
+    updateInterestThemes(){
+        let url = `/backend/api/protected/profile/${AuthService.getAuthToken().getCurrentProfile().entity.id}/interesting-in`;
+
+        this.frontlineService.session.auth.profiles[0].interesting_in = (JSON.parse(JSON.stringify(this.profileService.interestingIn)));
+
+        return this.http.put(url, JSON.stringify({
+            theme_ids: this.profileService.interestingIn
+        }));
+    }
+
 
     requestAccountDeleteCancel(){
         let url = `/backend/api/protected/account/cancel-request-delete`;
@@ -113,8 +75,8 @@ export class ProfileRESTService
         let url = `/backend/api/protected/account/change-password`;
 
         return this.http.post(url, JSON.stringify({
-            old_password: this.changePasswordStn.old_password,
-            new_password: this.changePasswordStn.new_password
+            old_password: this.profileService.changePasswordStn.old_password,
+            new_password: this.profileService.changePasswordStn.new_password
         }));
     }
 
