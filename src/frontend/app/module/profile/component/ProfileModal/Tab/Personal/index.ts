ca81e672902cc569e8e1486b8f2beb3a9e5f345c
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-23 17:31:05 +0300

    frontend / profile / Personal

--- src/frontend/app/module/profile/component/ProfileModal/Tab/Personal/index.ts
+++ src/frontend/app/module/profile/component/ProfileModal/Tab/Personal/index.ts
@@ -22,41 +22,6 @@ export class PersonalTab
         }
     }
 
-
-
-
-
-    /*returnActiveGreetings(){
-        let greetingsMethod = this.frontline.session.auth.profiles[0].greetings.greetings_method;
-        switch(greetingsMethod){
-            case 'fl':
-                this.greetings.firstname_lastname = true;
-                this.greetings.lastname_firstname_middlename = false;
-                this.greetings.middlename_firstname = false;
-                this.greetings.nickname = false;
-                break;
-            case 'n':
-                this.greetings.nickname = true;
-                this.greetings.firstname_lastname = false;
-                this.greetings.lastname_firstname_middlename = false;
-                this.greetings.middlename_firstname = false;
-                break;
-            case 'lfm':
-                this.greetings.lastname_firstname_middlename = true;
-                this.greetings.middlename_firstname = false;
-                this.greetings.nickname = false;
-                this.greetings.firstname_lastname = false;
-                break;
-            case 'mf':
-                this.greetings.middlename_firstname = true;
-                this.greetings.nickname = false;
-                this.greetings.firstname_lastname = false;
-                this.greetings.lastname_firstname_middlename = false;
-                break;
-        }
-    }*/
-
-
     chooseGreetings(greetMethod){
         let greetings = this.model.greetings;
         greetings.greetings_method = greetMethod;
