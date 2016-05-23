73255fd901243a92e4361f9c0b3922ca85d0cd3d
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-23 18:21:45 +0300

    frontend / profile / Personal

--- src/frontend/app/module/profile/component/ProfileModal/Tab/Personal/index.ts
+++ src/frontend/app/module/profile/component/ProfileModal/Tab/Personal/index.ts
@@ -16,14 +16,25 @@ export class PersonalTab
     constructor(public frontlineService: FrontlineService, private profileRESTService: ProfileRESTService, private profileService: ProfileService, private model: ProfileModalModel){}
 
 
-    returnActiveGreetings(value){
-        if(value === this.model.greetings.greetings_method){
+    getActiveSex(value){
+        if(value === this.model.profile.gender.string){
+            return true;
+        } else if(value === 'none'){
+            return true;
+        }
+    }
+
+    chooseSex(sex){
+        this.model.profile.gender.string = sex;
+    }
+
+    getActiveGreetings(value){
+        if(value === this.model.profile.greetings.greetings_method){
             return true;
         }
     }
 
     chooseGreetings(greetMethod){
-        let greetings = this.model.greetings;
-        greetings.greetings_method = greetMethod;
+        this.model.profile.greetings.greetings_method = greetMethod;
     }
 }
