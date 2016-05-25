ac17bf8ab5aa8957380aa7a1acdf563bf56cd456
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-25 22:29:02 +0300

    frontend / profile / ProfileSetupScreenScreenImage

--- src/frontend/app/module/profile/component/ProfileSetup/Screen/ProfileSetupScreenImage/index.ts
+++ src/frontend/app/module/profile/component/ProfileSetup/Screen/ProfileSetupScreenImage/index.ts
@@ -1,7 +1,16 @@
 import {Component, Injectable} from "angular2/core";
 import {CORE_DIRECTIVES} from "angular2/common";
+import {ImageCropperService} from "../../../../../util/component/ImageCropper/index";
+import {UploadImageService} from "../../../../../util/component/UploadImage/service";
+import {ImageCropper} from "../../../../../util/component/ImageCropper/index";
+import {ScreenControls} from "../../../../../util/classes/ScreenControls";
+import {UploadImageCropModel} from "../../../../../util/component/UploadImage/strategy";
 
-import {AvatarCropperService} from "../../../../../util/component/AvatarCropper/service";
+enum UploadImageScreen {
+    File = <any>"File",
+    Crop = <any>"Crop",
+    Processing = <any>"Processing"
+}
 
 @Component({
     selector: 'cass-profile-setup-screen-image',
@@ -10,29 +19,72 @@ import {AvatarCropperService} from "../../../../../util/component/AvatarCropper/
         require('./style.shadow.scss')
     ],
     providers: [
-        AvatarCropperService
+        ImageCropperService
     ],
     directives: [
         CORE_DIRECTIVES,
+        ImageCropper,
     ]
 })
+
 
 @Injectable()
 export class ProfileSetupScreenImage
 {
-    constructor(private avatarCropperService: AvatarCropperService){}
+    public progress = new UploadProgress();
 
-    isAvatarCropperVisible(){
-        return this.avatarCropperService.isAvatarFormVisibleFlag;
+    private screen: ScreenControls<UploadImageScreen> = new ScreenControls<UploadImageScreen>(UploadImageScreen.File, (sc: ScreenControls<UploadImageScreen>) => {
+        sc.add({ from: UploadImageScreen.File, to: UploadImageScreen.Crop });
+        sc.add({ from: UploadImageScreen.Crop, to: UploadImageScreen.Processing });
+    });
+
+    constructor(
+        public cropper: ImageCropperService) {}
+
+    private onFileChange($event) : void {
+        this.cropper.setFile($event.target.files[0]);
+        this.screen.goto(UploadImageScreen.Crop);
     }
 
-    showAvatarCropper() {
-        this.avatarCropperService.isAvatarFormVisibleFlag = true;
+    process() {
+        let model: UploadImageCropModel = {
+            x: this.cropper.getX(),
+            y: this.cropper.getY(),
+            width: this.cropper.getWidth(),
+            height: this.cropper.getHeight()
+        };
+
+        console.log(model);
+        this.screen.goto(UploadImageScreen.Processing);
     }
 
-    private onFileChange(event) : void {
-        this.avatarCropperService.file = event.target.files[0];
-        this.avatarCropperService.imgPath = URL.createObjectURL(event.target.files[0]);
-        this.showAvatarCropper();
+    abort() {
+        this.screen.goto(UploadImageScreen.Crop); //ToDo: Надо будет потом написать нормальный метод обрыва закачки.
+    }
+
+}
+
+class UploadProgress
+{
+    public value: number = 0;
+
+    public reset() {
+        this.value = 0;
+    }
+
+    abort() {
+        this.value = 0;
+    }
+
+    complete() {
+        this.value = 100;
+    }
+
+    update(value: number) {
+        if(value < 0 || value > 100) {
+            throw new Error(`Invalid progress ${value}`);
+        }
+
+        this.value = value;
     }
 }
