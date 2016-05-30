d4c2a24b2c8f1ea5eb7abbc24955cc639c61a5ea
PhilipMantrov <philip.mantrov@gmail.com>
2016-05-30 17:27:41 +0300

    frontend / profiele / ProfileRESTService

--- src/frontend/app/module/profile/component/ProfileService/ProfileRESTService.ts
+++ src/frontend/app/module/profile/component/ProfileService/ProfileRESTService.ts
@@ -7,16 +7,16 @@ import {AuthService} from "../../../auth/service/AuthService";
 import {FrontlineService} from "../../../frontline/service";
 
 @Injectable()
-export class ProfileRESTService
-{
-    constructor(public http: Http,
-                private frontlineService: FrontlineService){}
+export class ProfileRESTService {
+    constructor(public http:Http,
+                private frontlineService:FrontlineService) {
+    }
 
-    public tryNumber: number = 0;
-    public progressBar: number;
+    public tryNumber:number = 0;
+    public progressBar:number = 0;
 
 
-    editSex(profile){
+    editSex(profile) {
         let url = `/backend/api/protected/profile/${AuthService.getAuthToken().getCurrentProfile().entity.id}/set-gender`;
 
         this.frontlineService.session.auth.profiles[0].gender = profile.gender;
@@ -27,14 +27,14 @@ export class ProfileRESTService
         }))
     }
 
-    editPersonal(profile){
+    editPersonal(profile) {
         let url = `/backend/api/protected/profile/${AuthService.getAuthToken().getCurrentProfile().entity.id}/edit-personal/`;
 
         this.frontlineService.session.auth.profiles[0].greetings.greetings_method = profile.greetings.greetings_method;
-        this.frontlineService.session.auth.profiles[0].greetings.last_name =  profile.greetings.last_name;
-        this.frontlineService.session.auth.profiles[0].greetings.first_name =  profile.greetings.first_name;
-        this.frontlineService.session.auth.profiles[0].greetings.middle_name =  profile.greetings.middle_name;
-        this.frontlineService.session.auth.profiles[0].greetings.nickname =  profile.greetings.nickname;
+        this.frontlineService.session.auth.profiles[0].greetings.last_name = profile.greetings.last_name;
+        this.frontlineService.session.auth.profiles[0].greetings.first_name = profile.greetings.first_name;
+        this.frontlineService.session.auth.profiles[0].greetings.middle_name = profile.greetings.middle_name;
+        this.frontlineService.session.auth.profiles[0].greetings.nickname = profile.greetings.nickname;
 
 
         return this.http.post(url, JSON.stringify({
@@ -46,7 +46,7 @@ export class ProfileRESTService
         }));
     }
 
-    updateExpertThemes(expertIn){
+    updateExpertThemes(expertIn) {
         let url = `/backend/api/protected/profile/${AuthService.getAuthToken().getCurrentProfile().entity.id}/expert-in`;
 
         this.frontlineService.session.auth.profiles[0].expert_in = (JSON.parse(JSON.stringify(expertIn)));
@@ -56,7 +56,7 @@ export class ProfileRESTService
         }));
     }
 
-    updateInterestThemes(interestingIn){
+    updateInterestThemes(interestingIn) {
         let url = `/backend/api/protected/profile/${AuthService.getAuthToken().getCurrentProfile().entity.id}/interesting-in`;
 
         this.frontlineService.session.auth.profiles[0].interesting_in = (JSON.parse(JSON.stringify(interestingIn)));
@@ -67,19 +67,19 @@ export class ProfileRESTService
     }
 
 
-    requestAccountDeleteCancel(){
+    requestAccountDeleteCancel() {
         let url = `/backend/api/protected/account/cancel-request-delete`;
 
         return this.http.delete(url);
     }
 
-    requestAccountDelete(){
+    requestAccountDelete() {
         let url = `/backend/api/protected/account/request-delete`;
 
         return this.http.put(url, JSON.stringify(''));
     }
 
-    changePassword(changePasswordStn){
+    changePassword(changePasswordStn) {
         let url = `/backend/api/protected/account/change-password`;
 
         return this.http.post(url, JSON.stringify({
@@ -88,7 +88,21 @@ export class ProfileRESTService
         }));
     }
 
-    avatarUpload(file, model) {
+
+    signOut() {
+        let url = `/backend/api/auth/sign-out/`;
+        return this.http.get(url).subscribe(data => {
+            window.location.reload()
+        });
+    }
+
+    deleteAvatar() {
+        let url = `/backend/api/protected/profile/${AuthService.getAuthToken().getCurrentProfile().entity.id}/image-delete`;
+
+        return this.http.delete(url);
+    }
+
+    avatarUpload(file, model, modal) {
 
         let crop = {
             start: {
@@ -110,6 +124,8 @@ export class ProfileRESTService
         xmlRequest.upload.onprogress = (e) => {
             if (e.lengthComputable) {
                 this.progressBar = Math.floor((e.loaded / e.total) * 100);
+                modal.progress.update(this.progressBar);
+
             }
         };
 
@@ -118,12 +134,13 @@ export class ProfileRESTService
         xmlRequest.onreadystatechange = () => {
             if (xmlRequest.readyState === 4) {
                 if (xmlRequest.status === 200) {
-                    //this.avatarCropperService.isAvatarFormVisibleFlag = false;
                     this.frontlineService.session.auth.profiles[0].image.public_path = JSON.parse(xmlRequest.responseText).public_path;
+                    modal.progress.complete();
+                    modal.close();
                     this.progressBar = 0;
                     this.tryNumber = 0;
                 } else {
-                    this.avatarUpload(file, model);
+                    this.avatarUpload(file, model, modal);
                 }
             }
         }
