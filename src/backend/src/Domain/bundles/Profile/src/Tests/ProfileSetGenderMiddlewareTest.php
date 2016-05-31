0620747cd9f61f22c6b492398f09b51a09a65280
dborisenko <dborisenkowork@gmail.com>
2016-05-31 15:06:45 +0300

    backend / CASS-59 / random cmd+v

--- src/backend/src/Domain/bundles/Profile/src/Tests/ProfileSetGenderMiddlewareTest.php
+++ src/backend/src/Domain/bundles/Profile/src/Tests/ProfileSetGenderMiddlewareTest.php
@@ -129,7 +129,7 @@ class ProfileSetGenderMiddlewareTest extends ProfileMiddlewareTestCase
                         'string' => 'none'
                     ]
                 ]
-            ])backe
+            ])
         ;
     }
 
