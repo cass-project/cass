7d53f75752c59b25c51e8b849a44bdb5daecc2c7
dborisenkowork <dborisenkowork@gmail.com>
2016-06-06 12:10:53 +0300

    backend / post attachment middleware / fix, codestyle

--- src/backend/src/Domain/bundles/PostAttachment/src/Tests/PostAttachmentMiddlewareTest.php
+++ src/backend/src/Domain/bundles/PostAttachment/src/Tests/PostAttachmentMiddlewareTest.php
@@ -1,10 +1,10 @@
 <?php
-
-
 namespace Domain\PostAttachment\Tests;
+
 use Application\PHPUnit\RESTRequest\RESTRequest;
 use Application\PHPUnit\TestCase\MiddlewareTestCase;
 use Domain\Account\Tests\Fixtures\DemoAccountFixture;
+use Domain\PostAttachment\Entity\PostAttachment\File\ImageAttachmentType;
 use Zend\Diactoros\UploadedFile;
 
 /**
@@ -12,71 +12,64 @@ use Zend\Diactoros\UploadedFile;
  */
 class PostAttachmentMiddlewareTest extends MiddlewareTestCase
 {
-  protected function getFixtures(): array{
-   return [
-     new DemoAccountFixture()
-   ];
-  }
+    protected function getFixtures(): array {
+        return [
+            new DemoAccountFixture()
+        ];
+    }
 
-  public function testUpload200()
-  {
-    $localFilepath = __DIR__.'/Resources/grid-example.png';
+    public function testUpload200() {
+        $localFileName = __DIR__ . '/Resources/grid-example.png';
+        $uploadFile = new UploadedFile($localFileName, filesize($localFileName), 0);
 
-    $localFile = new UploadedFile($localFilepath, filesize($localFilepath), 0);
-    return $this->requestUpload($localFile)
-                ->auth(DemoAccountFixture::getAccount()->getAPIKey())
-                ->execute()
-      ->expectStatusCode(200)
-      ->expectJSONContentType()
-      ->expectJSONBody(['success' => true,])
+        return $this->requestUpload($uploadFile)
+            ->auth(DemoAccountFixture::getAccount()->getAPIKey())
+            ->execute()
+            ->expectStatusCode(200)
+            ->expectJSONContentType()
+            ->expectJSONBody(['success' => true]);
+    }
 
-      ;
-  }
-  public function testUploadBigFile409()
-  {
-    $localFile = __DIR__.'/Resources/grid-example.png';
+    public function testUploadBigFile409() {
+        $localFileName = __DIR__ . '/Resources/grid-example.png';
+        $uploadFile = new UploadedFile($localFileName, ImageAttachmentType::MAX_FILE_SIZE_BYTES + 1000, 0);
 
-    $localFile = new UploadedFile($localFile, 1024*1024*32+1000, 0);
+        return $this->requestUpload($uploadFile)
+            ->auth(DemoAccountFixture::getAccount()->getAPIKey())
+            ->execute()
+            ->expectStatusCode(409)
+            ->expectJSONContentType()
+            ->expectJSONBody(['success' => false]);
+    }
 
-    return $this->requestUpload($localFile)
-                ->auth(DemoAccountFixture::getAccount()->getAPIKey())
-                ->execute()
-      ->expectStatusCode(409)
-      ->expectJSONContentType()
-      ->expectJSONBody(['success' => false,])
-      ;
-  }
-  public function testUploadSmallFile409()
-  {
-    $localFile = __DIR__.'/Resources/grid-example.png';
+    public function testUploadSmallFile409() {
+        $localFileName = __DIR__ . '/Resources/grid-example.png';
+        $uploadFile = new UploadedFile($localFileName, 0, 0);
 
-    $localFile = new UploadedFile($localFile, 0, 0);
+        return $this->requestUpload($uploadFile)
+            ->auth(DemoAccountFixture::getAccount()->getAPIKey())
+            ->execute()
+            ->expectStatusCode(409)
+            ->expectJSONContentType()
+            ->expectJSONBody(['success' => false]);
+    }
 
-    return $this->requestUpload($localFile)
-                ->auth(DemoAccountFixture::getAccount()->getAPIKey())
-                ->execute()
-      ->expectStatusCode(409)
-      ->expectJSONContentType()
-      ->expectJSONBody(['success' => false,])
-      ;
-  }
+    public function testUpload403() {
+        $localFileName = __DIR__ . '/Resources/grid-example.png';
+        $uploadFile = new UploadedFile($localFileName, filesize($localFileName), 0);
 
-  public function testUpload403()
-  {
-    $localFile = __DIR__.'/Resources/grid-example.png';
+        return $this
+            ->requestUpload($uploadFile)
+            ->execute()
+            ->expectAuthError()
+        ;
+    }
 
-    $localFile = new UploadedFile($localFile, filesize($localFile), 0);
-
-    return $this->requestUpload($localFile)
-      ->execute()->expectAuthError();
-  }
-
-
-  protected function requestUpload( UploadedFile $localFile):RESTRequest
-  {
-    return $this->request('POST','/protected/post-attachment/upload')
-      ->setUploadedFiles([
-                           'file' => $localFile
-                         ]);
-  }
+    protected function requestUpload(UploadedFile $localFile):RESTRequest {
+        return $this
+            ->request('POST', '/protected/post-attachment/upload')
+            ->setUploadedFiles([
+                'file' => $localFile
+        ]);
+    }
 }
