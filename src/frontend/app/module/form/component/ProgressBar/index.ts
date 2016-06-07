cac13696220503f9f983b54ae75532d13729be0d
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-07 19:34:40 +0300

    frontend / form / ProgressBar

--- src/frontend/app/module/form/component/ProgressBar/index.ts
+++ src/frontend/app/module/form/component/ProgressBar/index.ts
@@ -1,3 +1,12 @@
-/**
- * Created by NiiNii on 6/7/2016.
- */
+import {Component, Input} from "angular2/core";
+
+@Component({
+    selector: 'cass-progress-bar',
+    template: require('./template.html')
+})
+export class ProgressBar
+{
+    @Input ('percent') percent: number = 0;
+    @Input ('color') color: string = 'black';
+    @Input ('type') type: string = 'default';
+}
