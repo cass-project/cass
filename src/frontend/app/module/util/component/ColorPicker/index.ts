bc38e4b9d820c5c5de8371138ef92e7223f6ca65
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-07 19:35:28 +0300

    frontend / util / ColorPicker

--- src/frontend/app/module/util/component/ColorPicker/index.ts
+++ src/frontend/app/module/util/component/ColorPicker/index.ts
@@ -1,3 +1,15 @@
-/**
- * Created by NiiNii on 6/7/2016.
- */
+import {Component, Input, Output, EventEmitter} from "angular2/core";
+import {FrontlineService} from "../../../frontline/service";
+
+@Component({
+    selector: 'cass-progress-bar',
+    template: require('./template.html')
+})
+export class ColorPicker
+{
+    constructor(private frontline: FrontlineService){}
+
+    @Input ('value') percent: number = 0;
+    @Input ('allow-none') color: string = 'black';
+    @Output ('change') change: any;
+}
