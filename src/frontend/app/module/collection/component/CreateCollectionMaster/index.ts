eb68f8caaad4754798317db5297bd9e6db341a54
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-08 15:30:22 +0300

    frontend / collection / CreateCollectionMaster

--- src/frontend/app/module/collection/component/CreateCollectionMaster/index.ts
+++ src/frontend/app/module/collection/component/CreateCollectionMaster/index.ts
@@ -1,3 +1,20 @@
-/**
- * Created by NiiNii on 6/8/2016.
- */
+import {Component, Input, Output, EventEmitter} from "angular2/core";
+import {ColorPicker} from "../../../util/component/ColorPicker/index";
+
+@Component({
+    selector: 'cass-collection-create-master',
+    template: require('./template.html'),
+    styles: [
+        require('./style.shadow.scss')
+    ],
+    directives: [
+        ColorPicker
+    ]
+})
+export class CreteCollectionMaster
+{
+    @Input ('for') _for: string;
+    @Input ('for-id') for_id: number;
+    @Output ('complete') complete = new EventEmitter();
+    @Output ('error') error = new EventEmitter();
+}
