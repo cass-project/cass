905516f3b2b3c4976716d44f6dbf33f94ae8497d
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-09 19:18:03 +0300

    frontend / profile / Routes / ProfileNotFound

--- src/frontend/app/module/profile/route/ProfileNotFound/index.ts
+++ src/frontend/app/module/profile/route/ProfileNotFound/index.ts
@@ -1,3 +1,17 @@
-/**
- * Created by NiiNii on 6/9/2016.
- */
+import {Component} from "angular2/core";
+import {RouteConfig, RouterOutlet} from "angular2/router";
+import {MessageBusService} from "../../../message/service/MessageBusService/index";
+import {AuthService} from "../../../auth/service/AuthService";
+import {MessageBusNotificationsLevel} from "../../../message/component/MessageBusNotifications/model";
+
+@Component({
+    template: require('./template.html'),
+    styles: [
+        require('./style.shadow.scss')
+    ]
+})
+export class ProfileNotFound
+{
+    constructor(private messageBus: MessageBusService){
+    }
+}
