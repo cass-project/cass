8dc03528d5952857b7d1607b2575051b08e234af
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-09 19:18:45 +0300

    frontend / profile / Routes

--- src/frontend/app/module/profile/route/ProfileRoute/index.ts
+++ src/frontend/app/module/profile/route/ProfileRoute/index.ts
@@ -1,12 +1,18 @@
 import {Component} from "angular2/core";
 
-import {RouterOutlet} from "angular2/router";
-import {RouteConfig} from "angular2/router";
+import {RouterOutlet, RouteConfig, Router} from "angular2/router";
 import {ModalComponent} from "../../../modal/component/index";
 import {ProfileMenuComponent} from "../../component/ProfileMenu/index";
 import {ProfileModal} from "../../component/ProfileModal/index";
 import {ProfileDashboardRoute} from "../ProfileDashboardRoute/index";
 import {ModalBoxComponent} from "../../../modal/component/box/index";
+import {ProfileNotFound} from "../ProfileNotFound/index";
+import {ProfileCurrentRoute} from "../ProfileCurrentRoute/index";
+import {ProfileCurrentCollectionsRoute} from "../ProfileCurrentCollectionsRoute/index";
+import {ProfileIDRoute} from "../ProfileIDRoute/index";
+import {ProfileIDCollectionsRoute} from "../ProfileIDCollectionsRoute/index";
+import {FrontlineService} from "../../../frontline/service";
+import {AuthService} from "../../../auth/service/AuthService";
 
 @Component({
     template: require('./template.html'),
@@ -27,8 +33,46 @@ import {ModalBoxComponent} from "../../../modal/component/box/index";
         path: '/',
         component: ProfileDashboardRoute,
         useAsDefault: true
+    },
+    {
+        name: 'ProfileIDRoute',
+        path: '/:sid',
+        component: ProfileIDRoute,
+    },
+    {
+        name: 'ProfileColletions',
+        path: '/:sid/collections',
+        component: ProfileIDCollectionsRoute,
+    },
+    {
+        name: 'ProfileColletionsById',
+        path: '/:sid/collections/:sid',
+        component: ProfileIDCollectionsRoute,
+    },
+    {
+        name: 'ProfileNotFound',
+        path: '/not-found',
+        component: ProfileNotFound
+    },
+    {
+        name: 'ProfileCurrentRoute',
+        path: '/current',
+        component: ProfileCurrentRoute
+    },
+    {
+        name: 'ProfileCurrentCollectionsRoute',
+        path: '/current/collections',
+        component: ProfileCurrentCollectionsRoute
+    },
+    {
+        name: 'ProfileCurrentCollectionsRouteSid',
+        path: '/current/collections/:sid',
+        component: ProfileCurrentCollectionsRoute
     }
 ])
 export class ProfileRoute
 {
+    /*constructor(private router: Router){
+        this.router.navigate(['ProfileCurrentCollectionsRoute']); /!*ToDo: редирект на дефолтную коллекцию*!/
+    }*/
 }
