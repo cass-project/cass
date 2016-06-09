e977cfcec5b99f384895763cfb89b5e59ce48366
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-09 19:17:29 +0300

    frontend / profile / Routes / ProfileCurrentRoute

--- src/frontend/app/module/profile/route/ProfileCurrentRoute/index.ts
+++ src/frontend/app/module/profile/route/ProfileCurrentRoute/index.ts
@@ -2,6 +2,7 @@ import {Component} from "angular2/core";
 import {RouteConfig, RouterOutlet} from "angular2/router";
 import {MessageBusService} from "../../../message/service/MessageBusService/index";
 import {AuthService} from "../../../auth/service/AuthService";
+import {MessageBusNotificationsLevel} from "../../../message/component/MessageBusNotifications/model";
 
 
 @Component({
@@ -12,11 +13,13 @@ import {AuthService} from "../../../auth/service/AuthService";
 })
 export class  ProfileCurrentRoute
 {
-    constructor(private messageBus: MessageBusService){}
+    constructor(private messageBus: MessageBusService){
+        this.getSignIn();
+    }
 
-    //getSignIn(){
-    //    if(!AuthService.isSignedIn()){
-    //        this.messageBus.push(info, "Вы не авторизованы");
-    //    }
-    //}
+    getSignIn(){
+        if(!AuthService.isSignedIn()){
+            this.messageBus.push(MessageBusNotificationsLevel.Info, "Вы не авторизованы");
+        }
+    }
 }
