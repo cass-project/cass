773bb0483221780f916e5e276a7e72992480e533
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-09 19:18:17 +0300

    frontend / profile / Routes / ProfileIDRoute

--- src/frontend/app/module/profile/route/ProfileIDRoute/index.ts
+++ src/frontend/app/module/profile/route/ProfileIDRoute/index.ts
@@ -1,8 +1,9 @@
 import {Component} from "angular2/core";
-import {RouteConfig, RouterOutlet, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouteParams} from "angular2/router";
+import {RouteConfig, RouterOutlet, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouteParams, Router} from "angular2/router";
 import {MessageBusService} from "../../../message/service/MessageBusService/index";
 import {AuthService} from "../../../auth/service/AuthService";
 import {MessageBusNotificationsLevel} from "../../../message/component/MessageBusNotifications/model";
+import {ProfileRESTService} from "../../component/ProfileService/ProfileRESTService";
 
 
 @Component({
@@ -11,12 +12,31 @@ import {MessageBusNotificationsLevel} from "../../../message/component/MessageBu
         require('./style.shadow.scss')
     ]
 })
-export class ProfileID
+export class ProfileIDRoute
 {
-    constructor(private messageBus: MessageBusService, params: RouteParams){
-        this.sid = params.get('sid');
-        console.log(this.sid);
-    }
-
     sid: string;
+
+    constructor(private messageBus: MessageBusService, params: RouteParams, private profileRESTService: ProfileRESTService, private router: Router){
+
+        this.sid = params.get('sid');
+        this.profileRESTService.getProfileById(this.sid).subscribe(
+            data => {
+            console.log(data);
+            },
+            err => {
+                console.log(err.status);
+                if(err.status === 404){
+                    this.router.navigate(['ProfileNotFound']);
+                } else if(err.status === 403){
+                    this.router.navigateByUrl('/auth/sign-in');
+                    this.messageBus.push(MessageBusNotificationsLevel.Warning, 'Вы не авторизованы');
+                } else if(err.status){
+                    this.messageBus.push(MessageBusNotificationsLevel.Critical, JSON.parse(err._body).error);
+                    console.log();
+                } else {
+                    this.messageBus.push(MessageBusNotificationsLevel.Warning, 'Произошла ошибка. Похоже, у вас проблемы с интернетом!');
+                }
+            }
+        );
+    }
 }
