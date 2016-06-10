84581b4eaace88defc210cac0dd0ec6cf01c9c2c
hck <art.hacker@ya.ru>
2016-06-10 13:30:21 +0300

    frontend /community / settings / верстка

--- src/frontend/app/module/community/component/Modal/CommunitySettingsModal/Tab/TabGeneral/index.ts
+++ src/frontend/app/module/community/component/Modal/CommunitySettingsModal/Tab/TabGeneral/index.ts
@@ -17,16 +17,61 @@ import {CommunityResponseModel, CommunityModel} from "../../../../../model";
 
 export class GeneralTab {
 
+    private deleteRequest: CommunityDeleteRequestControls = new CommunityDeleteRequestControls();
+    private isCommunitySettingsModalThemeEnabled: boolean;
+    private isCommunitySettingsModalThemeEnablePublic: boolean;
+    private isCommunitySettingsModalThemeWillModerate: boolean;
+
     constructor(protected model: CommunityModel, protected responseModel: CommunityResponseModel) {
         this.model = responseModel.entity;
     }
 
-    updateThemeId(themeIds: number[]) {
-        this.model.theme_id = themeIds[0];
+    communitySettingsModalThemeEnabledChange($event: boolean) {
+        if($event===false) {
+            this.isCommunitySettingsModalThemeEnablePublic = false;
+            this.isCommunitySettingsModalThemeWillModerate = false;
+        }
+    }
+}
+
+class CommunityDeleteRequestControls
+{
+    private buttonDisabled: boolean = false;
+    public stage: DeleteRequestStage = DeleteRequestStage.NoRequest;
+
+    request() {
+        this.buttonDisabled = true;
+
+        setTimeout(() => {
+            this.stage = DeleteRequestStage.PendingConfirmation;
+            this.buttonDisabled = false;
+        }, 1000);
     }
 
-    unsetTheme(){
-        this.model.theme_id = null;
+    confirm() {
+        this.stage = DeleteRequestStage.Confirmed;
     }
 
+    cancel() {
+        this.stage = DeleteRequestStage.NoRequest;
+    }
+
+    isButtonDisabled() {
+        return this.buttonDisabled;
+    }
+
+    isRequested() {
+        return this.stage === DeleteRequestStage.PendingConfirmation;
+    }
+
+    isConfirmed() {
+        return this.stage === DeleteRequestStage.Confirmed;
+    }
+}
+
+enum DeleteRequestStage
+{
+    NoRequest,
+    PendingConfirmation,
+    Confirmed
 }
