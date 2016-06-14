2edb3e7674ca590ac07bd749f58991c55f534e68
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-14 19:39:58 +0300

    frontend / collection / CollectionRESTService

--- src/frontend/app/module/collection/service/CollectionRESTService.ts
+++ src/frontend/app/module/collection/service/CollectionRESTService.ts
@@ -1,3 +1,23 @@
-/**
- * Created by NiiNii on 6/14/2016.
- */
+import {Injectable} from 'angular2/core';
+import {Http} from 'angular2/http';
+
+@Injectable()
+export class CollectionRESTService {
+    constructor(public http:Http) {}
+
+    profileCreateCollection(profileId, collection){
+        let url = `/backend/api/protected/profile/collection/create`;
+
+        return this.http.put(url, JSON.stringify({
+            author_profile_id: profileId,
+            theme_id: collection.theme_id,
+            title: collection.title,
+            description: collection.description
+        }));
+
+    }
+
+    collectionCreateCollection(){
+
+    }
+}
