033cf6545e84e75432a3090c67e8b009320de15d
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-18 02:33:53 +0300

    frontend / collection / CollectionSettings

---
+++ src/frontend/app/module/collection/component/CollectionSettings/index.ts
@@ -0,0 +1,34 @@
+import {Component} from "angular2/core";
+
+import {ModalComponent} from "../../../modal/component/index";
+import {ModalBoxComponent} from "../../../modal/component/box/index";
+import {TAB_MODAL_DIRECTIVES} from "../../../form/component/TabModal/index";
+import {ColorPicker} from "../../../util/component/ColorPicker/index";
+import {ThemeSelect} from "../../../theme/component/ThemeSelect/index";
+import {CollectionImage} from "../../../collection/component/Elements/CollectionImage/index";
+import {DeleteCollectionModal} from "../../../collection/component/Modal/DeleteCollectionModal/index";
+import {ModalControl} from "../../../util/classes/ModalControl";
+
+@Component({
+    template: require('./template.jade'),
+    styles: [
+        require('./style.shadow.scss')
+    ],
+    directives: [
+        ModalComponent,
+        ModalBoxComponent,
+        TAB_MODAL_DIRECTIVES,
+        ColorPicker,
+        ThemeSelect,
+        CollectionImage,
+        DeleteCollectionModal,
+    ]
+})
+export class CollectionSettingsDemo
+{
+    private deleteModal: ModalControl = new ModalControl();
+
+    requestDeleteCollection() {
+        this.deleteModal.open();
+    }
+}
