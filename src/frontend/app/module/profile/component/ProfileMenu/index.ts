fe643f84b256c83a422e33a209d82ab62a95dd7e
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-18 02:34:10 +0300

    frontend / profile / ProfileMenu

--- src/frontend/app/module/profile/component/ProfileMenu/index.ts
+++ src/frontend/app/module/profile/component/ProfileMenu/index.ts
@@ -1,4 +1,10 @@
 import {Component, Input, Output, EventEmitter} from "angular2/core";
+import {Router} from "angular2/router";
+import {ProfileRESTService} from "../../service/ProfileRESTService";
+import {ProfileComponent} from "../../index";
+import {AuthService} from "../../../auth/service/AuthService";
+import {FrontlineService} from "../../../frontline/service";
+import {ProfileComponentService} from "../../service";
 
 @Component({
     selector: 'cass-profile-menu',
@@ -12,9 +18,14 @@ export class ProfileMenuComponent
     @Input('profile') profile: any;
     @Output('create_collection') create_collection = new EventEmitter();
 
-    ProfileCollectionsList;
+    currentProfile;
 
-    constructor(){
 
+
+
+    constructor(private profileRESTService: ProfileRESTService, private router: Router, private pService: ProfileComponentService){
+        if(AuthService.isSignedIn()) {
+            this.currentProfile = AuthService.getAuthToken().getCurrentProfile().entity;
+        } else this.router.navigate(['Landing']);
     }
 }
