c186777d4304ae1f47f44701f7b6c8f2c15e1333
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-19 21:13:36 +0300

    frontend / collection / CollectionSettings

--- src/frontend/app/module/collection/component/CollectionSettings/index.ts
+++ src/frontend/app/module/collection/component/CollectionSettings/index.ts
@@ -1,4 +1,4 @@
-import {Component} from "angular2/core";
+import {Component, Input} from "angular2/core";
 
 import {ModalComponent} from "../../../modal/component/index";
 import {ModalBoxComponent} from "../../../modal/component/box/index";
@@ -8,6 +8,7 @@ import {ThemeSelect} from "../../../theme/component/ThemeSelect/index";
 import {CollectionImage} from "../../../collection/component/Elements/CollectionImage/index";
 import {DeleteCollectionModal} from "../../../collection/component/Modal/DeleteCollectionModal/index";
 import {ModalControl} from "../../../util/classes/ModalControl";
+import {ProfileComponentService} from "../../../profile/service";
 
 @Component({
     template: require('./template.jade'),
@@ -27,6 +28,12 @@ import {ModalControl} from "../../../util/classes/ModalControl";
 })
 export class CollectionSettings
 {
+
+    @Input('collection') collection;
+
+    constructor(private pService: ProfileComponentService){
+        console.log(this.collection);
+    }
 
     private deleteModal: ModalControl = new ModalControl();
 
