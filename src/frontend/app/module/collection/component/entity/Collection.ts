ad33850fb42be97e89ab7469a1b188969e95d3ac
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-19 21:13:08 +0300

    frontend / collection / Collection Interface

--- src/frontend/app/module/collection/component/entity/Collection.ts
+++ src/frontend/app/module/collection/component/entity/Collection.ts
@@ -1,3 +1,10 @@
-/**
- * Created by NiiNii on 6/19/2016.
- */
+export interface Collection
+{
+    author_profile_id: string,
+    owner_community_id: string,
+    title: string,
+    description: string,
+    theme_ids: Array<any>,
+    theme: {has: boolean},
+    image: {small: {public_path: string}}
+}
