3d56f6c9a4f7de7bfc16431344dc5a48b0c70138
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-19 21:13:23 +0300

    frontend / collection / CollectionRESTService

--- src/frontend/app/module/collection/service/CollectionRESTService.ts
+++ src/frontend/app/module/collection/service/CollectionRESTService.ts
@@ -5,23 +5,23 @@ import {Http} from 'angular2/http';
 export class CollectionRESTService {
     constructor(public http:Http) {}
 
-    profileCreateCollection(profileId, collection){
+    profileCreateCollection(collection){
         let url = `/backend/api/protected/profile/collection/create`;
 
         return this.http.put(url, JSON.stringify({
-            author_profile_id: profileId,
-            theme_id: collection.theme_id,
+            author_profile_id: collection.author_profile_id,
+            theme_id: 0,
             title: collection.title,
             description: collection.description
         }));
 
     }
 
-    communityCreateCollection(profileId, communityId, collection){
-        let url = `/backend/api/protected/community/${communityId}/collection/create`;
+    communityCreateCollection(collection){
+        let url = `/backend/api/protected/community/${collection.owner_community_id}/collection/create`;
 
         return this.http.put(url, JSON.stringify({
-            author_profile_id: profileId,
+            author_profile_id: collection.author_profile_id,
             theme_id: collection.theme_id,
             title: collection.title,
             description: collection.description
