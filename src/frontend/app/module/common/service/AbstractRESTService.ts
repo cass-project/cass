ca60fcc94384f4dc4975bac64f2f4ce74c0d82f2
dborisenkowork <dborisenkowork@gmail.com>
2016-06-21 20:08:02 +0300

    frontend / AbstractRESTService

--- src/frontend/app/module/common/service/AbstractRESTService.ts
+++ src/frontend/app/module/common/service/AbstractRESTService.ts
@@ -1,11 +1,11 @@
 import {Http, Response} from "angular2/http";
 import {Observable} from "rxjs/Observable";
 
-import {MessageBus} from "angular2/src/web_workers/shared/message_bus";
+import {MessageBusNotifications} from "../../message/component/MessageBusNotifications/index";
 
 export abstract class AbstractRESTService
 {
-    constructor(protected http: Http, private messages: MessageBus) {}
+    constructor(protected http: Http, private messages: MessageBusNotifications) {}
 
     handle(request: Observable<Response>): Observable<Response> {
         request.map(res => res.json())
