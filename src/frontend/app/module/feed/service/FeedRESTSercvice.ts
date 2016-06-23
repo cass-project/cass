cb3e0d03bb01b878600a9ade86ae62dead063402
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-23 17:48:21 +0300

    frontend / feed / FeedRESTService

--- src/frontend/app/module/feed/service/FeedRESTSercvice.ts
+++ src/frontend/app/module/feed/service/FeedRESTSercvice.ts
@@ -1,3 +1,24 @@
-/**
- * Created by NiiNii on 6/23/2016.
- */
+import {Injectable} from "angular2/core";
+import {Http} from "angular2/http"
+import {AbstractRESTService} from "../../common/service/AbstractRESTService";
+import {Account} from "../../account/definitions/entity/Account";
+import {MessageBusService} from "../../message/service/MessageBusService/index";
+
+@Injectable()
+export class FeedRESTSercvice extends AbstractRESTService {
+    constructor(protected  http:Http, protected messages:MessageBusService) {
+        super(http, messages);
+    }
+
+    getFeed(collectionId: number, criteria)
+    {
+        return this.handle(this.http.post(`/backend/api/feed/collection/${collectionId}`, JSON.stringify({
+            criteria: {
+                seek: {
+                    limit: criteria.seek.limit,
+                    offset: criteria.seek.offset
+                }
+            }
+        })));
+    }
+}
