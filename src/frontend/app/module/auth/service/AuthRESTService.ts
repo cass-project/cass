cbf05d560c9410fe829b709d4ac44abbd757cd0b
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-23 15:37:59 +0300

    frontend / auth / AuthRESTService

--- src/frontend/app/module/auth/service/AuthRESTService.ts
+++ src/frontend/app/module/auth/service/AuthRESTService.ts
@@ -1,23 +1,31 @@
+import {Injectable} from "angular2/core";
+import {Http} from "angular2/http"
 import {AbstractRESTService} from "../../common/service/AbstractRESTService";
 import {SignInRequest} from "../definitions/paths/sign-in";
 import {SignUpRequest} from "../definitions/paths/sign-up";
 import {AuthService} from "./AuthService";
 import {Account} from "../../account/definitions/entity/Account";
+import {MessageBusService} from "../../message/service/MessageBusService/index";
 
+@Injectable()
 export class AuthRESTService extends AbstractRESTService
 {
+    constructor(protected  http: Http, protected messages: MessageBusService) {
+        super(http, messages);
+    }
+
     signIn(request: SignInRequest)
     {
-        return this.handle(this.http.post("/auth/sign-in", JSON.stringify(request)));
+        return this.handle(this.http.post("/backend/api/auth/sign-in", JSON.stringify(request)));
     }
 
     signOut()
     {
-        return this.handle(this.http.get("/auth/sign-out"));
+        return this.handle(this.http.get("/backend/api/auth/sign-out"));
     }
 
     signUp(request: SignUpRequest)
     {
-        return this.handle(this.http.put("/auth/sign-up", JSON.stringify(request)));
+        return this.handle(this.http.put("/backend/api/auth/sign-up", JSON.stringify(request)));
     }
 }
