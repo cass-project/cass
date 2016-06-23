72a003feeb6bfe6a65f26bf537839fc7aeca8108
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-23 17:48:10 +0300

    frontend / email-verification / EmailVerificationRESTService

--- src/frontend/app/module/email-verification/service/EmailVerificationRESTService.ts
+++ src/frontend/app/module/email-verification/service/EmailVerificationRESTService.ts
@@ -1,3 +1,22 @@
-/**
- * Created by NiiNii on 6/23/2016.
- */
+import {Injectable} from "angular2/core";
+import {Http} from "angular2/http"
+import {AbstractRESTService} from "../../common/service/AbstractRESTService";
+import {Account} from "../../account/definitions/entity/Account";
+import {MessageBusService} from "../../message/service/MessageBusService/index";
+
+@Injectable()
+export class EmailVerificationRESTService extends AbstractRESTService {
+    constructor(protected  http:Http, protected messages:MessageBusService) {
+        super(http, messages);
+    }
+
+    emailVerification(token: string)
+    {
+        return this.handle(this.http.get(`/backend/api/email-verification/confirm/${token}`));
+    }
+
+    RequestEmailVerification(newEmail: string)
+    {
+        return this.handle(this.http.get(`/backend/api/email-verification/request/${newEmail}`));
+    }
+}
