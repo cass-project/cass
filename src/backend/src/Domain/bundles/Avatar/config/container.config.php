8c70d83c96bb4284520488a5ca3023f0264ba802
coffeeturbo <coffeeturbo@gmail.com>
2016-06-24 18:56:34 +0300

    backend / Avatar service / generate image - not ready

--- src/backend/src/Domain/bundles/Avatar/config/container.config.php
+++ src/backend/src/Domain/bundles/Avatar/config/container.config.php
@@ -1,6 +1,8 @@
 <?php
 namespace Domain\Colors;
 
+use Application\Service\BundleService;
+use DI\Container;
 use function DI\object;
 use function DI\factory;
 use function DI\get;
@@ -8,22 +10,35 @@ use function DI\get;
 use Domain\Avatar\Service\AvatarService;
 use Domain\Avatar\Service\Strategy\FileAvatarStrategy;
 use Domain\Avatar\Service\Strategy\MockAvatarStrategy;
+use Domain\DomainBundle;
 
 return [
     'env' => [
         'development' => [
             'php-di' => [
                 AvatarService::class => object()->constructorParameter('strategy', get(FileAvatarStrategy::class))
+                  ->constructorParameter('fontPath',factory(function(Container $container){
+                      return sprintf('%s/fonts/Roboto/Roboto-Medium.ttf',
+                                     $container->get(BundleService::class)->getBundleByName(DomainBundle::class )->getResourcesDir());
+                  }))
             ]
         ],
         'production' => [
             'php-di' => [
                 AvatarService::class => object()->constructorParameter('strategy', get(FileAvatarStrategy::class))
+                  ->constructorParameter('fontPath',factory(function(Container $container){
+                      return sprintf('%s/fonts/Roboto/Roboto-Medium.ttf',
+                                     $container->get(BundleService::class)->getBundleByName(DomainBundle::class )->getResourcesDir());
+                  }))
             ]
         ],
         'test' => [
             'php-di' => [
                 AvatarService::class => object()->constructorParameter('strategy', get(MockAvatarStrategy::class))
+                  ->constructorParameter('fontPath',factory(function(Container $container){
+                      return sprintf('%s/fonts/Roboto/Roboto-Medium.ttf',
+                                     $container->get(BundleService::class)->getBundleByName(DomainBundle::class )->getResourcesDir());
+                  }))
             ]
         ]
     ]
