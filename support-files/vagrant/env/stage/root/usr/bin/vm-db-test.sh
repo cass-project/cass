e9939a373268d0fd7e286d58694bd7a0c9cf68e1
PhilipMantrov <philip.mantrov@gmail.com>
2016-06-25 16:11:27 +0300

    path fix

--- support-files/vagrant/env/stage/root/usr/bin/vm-db-test.sh
+++ support-files/vagrant/env/stage/root/usr/bin/vm-db-test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find /support-files/sql/test -name "*.sql" -exec cat {} \; | mysql -uroot -p"1234" cass_development
+find /opt/cass/support-files/sql/test -name "*.sql" -exec cat {} \; | mysql -uroot -p"1234" cass_development
