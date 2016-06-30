2e4c2b4a68c13ddd3a7f803d937162f0e4616c80
hck <art.hacker@ya.ru>
2016-06-30 06:57:21 +0300

    frontend / feedback / create / add logic & css & html

--- src/frontend/src/module/feedback/component/Modal/FeedbackCreateModal/index.ts
+++ src/frontend/src/module/feedback/component/Modal/FeedbackCreateModal/index.ts
@@ -1,7 +1,16 @@
 import {Component} from "angular2/core";
+import {Location, Router} from 'angular2/router';
 
 import {ModalComponent} from "../../../../modal/component/index";
 import {ModalBoxComponent} from "../../../../modal/component/box/index";
+import {FrontlineService} from "../../../../frontline/service";
+import {FeedbackCreateModalModel} from "./model";
+import {FeedbackService} from "../../../service/FeedbackService";
+import {CreateFeedbackRequest} from "../../../definitions/paths/create";
+import {CurrentAccountService} from "../../../../auth/service/CurrentAccountService";
+import {MessageBusService} from "../../../../message/service/MessageBusService/index";
+import {MessageBusNotificationsLevel} from "../../../../message/component/MessageBusNotifications/model";
+import {ProgressLock} from "../../../../form/component/ProgressLock/index";
 
 @Component({
     selector: 'cass-feedback-create-modal',
@@ -12,9 +21,50 @@ import {ModalBoxComponent} from "../../../../modal/component/box/index";
     directives: [
         ModalComponent,
         ModalBoxComponent,
+        ProgressLock
     ]
 })
 
 export class FeedbackCreateModal
 {
+    private feedbackTypes:any[] = [];
+    private isLoading:boolean = false;
+    constructor(
+        public model: FeedbackCreateModalModel,
+        private router: Router,
+        private frontline: FrontlineService,
+        private service: FeedbackService,
+        private messages: MessageBusService,
+        private accountService: CurrentAccountService
+)
+    {
+        this.feedbackTypes = frontline.session.config.feedback.types;
+    }
+    
+    abort() {
+    }
+    
+    submit() {
+        this.isLoading = true;
+        this.service.create(<CreateFeedbackRequest>{
+            profile_id: this.accountService.getCurrentProfile().getId(),
+            type_feedback: this.model.type_feedback,
+            description: this.model.description
+        }).subscribe(
+            data => {
+                this.messages.push(MessageBusNotificationsLevel.Info, "Отзыв успешно отправлен!");
+                this.router.navigateByUrl("/");
+                this.isLoading = false;
+            },
+            error => {
+                error._body = JSON.parse(error._body);
+                if(error.status === 404) {
+                    this.messages.push(MessageBusNotificationsLevel.Critical, error._body.error)
+                } else {
+                    console.log(error);
+                }
+                this.isLoading = false;
+            }
+        );
+    }
 }
