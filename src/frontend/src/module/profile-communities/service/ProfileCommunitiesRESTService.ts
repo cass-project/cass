0194fda5309440927383d86e0595608b75d3b569
PhilipMantrov <philip.mantrov@gmail.com>
2016-07-01 17:07:10 +0300

    frontend / ProfileCommunitiesRESTService

--- src/frontend/src/module/profile-communities/service/ProfileCommunitiesRESTService.ts
+++ src/frontend/src/module/profile-communities/service/ProfileCommunitiesRESTService.ts
@@ -1,9 +1,7 @@
 import {Injectable} from "angular2/core";
-import {Http} from "angular2/http"
+import {Http, Headers} from "angular2/http"
 import {AbstractRESTService} from "../../common/service/AbstractRESTService";
-import {Account} from "../../account/definitions/entity/Account";
 import {MessageBusService} from "../../message/service/MessageBusService/index";
-import {AuthService} from "../../auth/service/AuthService";
 import {AuthToken} from "../../auth/service/AuthToken";
 
 @Injectable()
@@ -17,14 +15,29 @@ export class ProfileCommunitiesRESTService extends AbstractRESTService
 
     getJoinedCommunities()
     {
-        return this.handle(this.http.get(`/backend/api/protected/profile/current/joined-communities`));
+        let authHeader = new Headers();
+        if(this.token.hasToken()){
+            authHeader.append('Authorization', `${this.token.apiKey}`);
+        }
+        
+        return this.handle(this.http.get(`/backend/api/protected/profile/current/joined-communities`, {headers: authHeader}));
     }
 
     joinCommunity(communitySID){
-        return this.handle(this.http.put(`/backend/api/protected/community/${communitySID}/join`, JSON.stringify({})));
+        let authHeader = new Headers();
+        if(this.token.hasToken()){
+            authHeader.append('Authorization', `${this.token.apiKey}`);
+        }
+        
+        return this.handle(this.http.put(`/backend/api/protected/community/${communitySID}/join`, '', {headers: authHeader}));
     }
 
     leaveCommunity(communitySID){
-        return this.handle(this.http.put(`/backend/api/protected/community/${communitySID}/leave`, JSON.stringify({})));
+        let authHeader = new Headers();
+        if(this.token.hasToken()){
+            authHeader.append('Authorization', `${this.token.apiKey}`);
+        }
+        
+        return this.handle(this.http.put(`/backend/api/protected/community/${communitySID}/leave`, '', {headers: authHeader}));
     }
 }
