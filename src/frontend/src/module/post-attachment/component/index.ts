69343b8c7dc194baf6a734d30988b02d3435f10b
PhilipMantrov <philip.mantrov@gmail.com>
2016-07-01 05:56:58 +0300

    frontend / post-attachment

--- src/frontend/src/module/post-attachment/component/index.ts
+++ src/frontend/src/module/post-attachment/component/index.ts
@@ -1,10 +1,7 @@
 import {Component, Input, Output, EventEmitter, Injectable} from "angular2/core";
-import {PostEntity} from "../definitions/entity/Post";
-import {CreatePostRequest} from "../definitions/paths/create";
-import {CurrentAccountService} from "../../auth/service/CurrentAccountService";
 
 @Component({
-    selector: 'cass-post-form',
+    selector: 'cass-post-form-attachment',
     template: require('./template.jade'),
     styles: [
         require('./style.shadow.scss')
@@ -12,29 +9,7 @@ import {CurrentAccountService} from "../../auth/service/CurrentAccountService";
 })
 
 @Injectable()
-export class PostForm
+export class PostAttachment
 {
-    constructor(private currentAccountService: CurrentAccountService)
-    {
-        this.postEntity.author_profile_id = this.currentAccountService.getCurrentProfile().getId();
-    }
 
-    attachFile: boolean = false;
-
-    @Input('collectionId') collectionId;
-
-    @Output('postEvent') postEvent = new EventEmitter<PostEntity>();
-
-    postEntity: PostEntity;
-
-    submit()
-    {
-        this.postEntity.date_created_on = new Date().toLocaleDateString();
-
-    }
-
-    openAttachModal()
-    {
-        this.attachFile = true;
-    }
 }
