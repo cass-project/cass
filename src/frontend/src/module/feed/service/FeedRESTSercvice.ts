58491f0f18a8f48b807a4957c91bc916d3ff02f9
PhilipMantrov <philip.mantrov@gmail.com>
2016-07-01 17:06:33 +0300

    frontend / FeedRESTService

--- src/frontend/src/module/feed/service/FeedRESTSercvice.ts
+++ src/frontend/src/module/feed/service/FeedRESTSercvice.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "angular2/core";
-import {Http} from "angular2/http"
+import {Http, Headers} from "angular2/http"
 import {AbstractRESTService} from "../../common/service/AbstractRESTService";
 import {Account} from "../../account/definitions/entity/Account";
 import {MessageBusService} from "../../message/service/MessageBusService/index";
@@ -13,10 +13,15 @@ export class FeedRESTSercvice extends AbstractRESTService
         protected http: Http,
         protected token: AuthToken,
         protected messages: MessageBusService
-    ) { super(http, token, messages); }
+    ) {super(http, token, messages)}
 
     getFeed(collectionId: number, criteria)
     {
+        let authHeader = new Headers();
+        if(this.token.hasToken()){
+            authHeader.append('Authorization', `${this.token.apiKey}`);
+        }
+        
         return this.handle(this.http.post(`/backend/api/feed/collection/${collectionId}`, JSON.stringify({
             criteria: {
                 seek: {
@@ -24,6 +29,6 @@ export class FeedRESTSercvice extends AbstractRESTService
                     offset: criteria.seek.offset
                 }
             }
-        })));
+        }), {headers: authHeader}));
     }
 }
