52d3f62fecc1b4d20ea9f1a6eeaa246e118b9de8
PhilipMantrov <philip.mantrov@gmail.com>
2016-07-01 17:06:41 +0300

    frontend / FeedbackRESTService

--- src/frontend/src/module/feedback/service/FeedbackRESTService.ts
+++ src/frontend/src/module/feedback/service/FeedbackRESTService.ts
@@ -1,16 +1,26 @@
 import {Injectable} from "angular2/core";
-import {Http, Response} from "angular2/http";
+import {Http, Response, Headers} from "angular2/http";
 import {Observable} from "rxjs/Rx";
 
 import {FeedbackCreateRequest} from "../definitions/paths/create";
+import {AbstractRESTService} from "../../common/service/AbstractRESTService";
+import {AuthToken} from "../../auth/service/AuthToken";
+import {MessageBusService} from "../../message/service/MessageBusService/index";
 
 @Injectable()
-export class FeedbackRESTService
+export class FeedbackRESTService extends AbstractRESTService
 {
-    constructor(private http: Http) {}
+    constructor(protected http: Http,
+                protected token: AuthToken,
+                protected messages: MessageBusService) {super(http, token, messages)}
     
     public create(request: FeedbackCreateRequest) : Observable<Response>
     {
-        return this.http.put("/backend/api/feedback/create", JSON.stringify(request));
+        let authHeader = new Headers();
+        if(this.token.hasToken()){
+            authHeader.append('Authorization', `${this.token.apiKey}`);
+        }
+
+        return this.handle(this.http.put("/backend/api/feedback/create", JSON.stringify(request), {headers: authHeader}));
     }
 }
