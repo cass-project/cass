637c7fadf3900995fb36daeb8a105987b6acfecb
hck <art.hacker@ya.ru>
2016-07-02 03:30:17 +0300

    frontend / app / feedback-app / add bootstrap 4 theme

--- src/frontend/src/app/feedback-app/src/module/landing/index.ts
+++ src/frontend/src/app/feedback-app/src/module/landing/index.ts
@@ -1,11 +1,7 @@
 import {Component} from "angular2/core";
-import {RouterOutlet} from "angular2/router";
 
 @Component({
     selector: 'cass-feedback-landing',
     template: require('./template.jade'),
-    directives: [
-        RouterOutlet
-    ],
 })
-export class IndexComponent {}
+export class LandindComponent {}
