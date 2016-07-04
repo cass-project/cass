a643f27989e0a51de1c8d07628c7359c940ab4a0
PhilipMantrov <philip.mantrov@gmail.com>
2016-07-04 23:57:01 +0300

    frontend / post

--- src/frontend/src/module/post/component/index.ts
+++ src/frontend/src/module/post/component/index.ts
@@ -1,19 +1,28 @@
-import {Component} from "angular2/core";
+import {Component, Injectable, Input, Output, EventEmitter} from "angular2/core";
+import {PostRESTService} from "../service/PostRESTService";
 
 @Component({
-    selector: 'cass-post-form-demo',
+    selector: 'cass-post-form',
     template: require('./template.jade'),
     styles: [
         require('./style.shadow.scss')
     ],
 })
-export class PostFormDemo 
+
+@Injectable()
+export class PostForm
 {
+    constructor(private postRESTService: PostRESTService){}
+
     isFocused: boolean;
     model: PostFormModel = {
         content: ''
     };
-    
+
+    @Input('collection_id') collection_id: number;
+    @Output('postEvent') postEvent = new EventEmitter();
+
+
     isExpanded(): boolean {
         let testHasContent = this.model.content.length > 0;
         let testIsFocused = this.isFocused;
