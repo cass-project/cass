778a6487a6a3baac34c2e1af7e12f561b98f73fd
dborisenkowork <dborisenkowork@gmail.com>
2016-07-06 02:41:31 +0300

    backend / post / post-theme-eq

--- src/backend/src/Domain/bundles/Post/config/container.config.php
+++ src/backend/src/Domain/bundles/Post/config/container.config.php
@@ -5,16 +5,15 @@ use function DI\object;
 use function DI\factory;
 use function DI\get;
 
-use Domain\Auth\Service\CurrentAccountService;
 use Application\Doctrine2\Factory\DoctrineRepositoryFactory;
 use Domain\Post\Entity\Post;
-use Domain\Post\Middleware\PostMiddleware;
+use Domain\Post\Entity\PostThemeEQ;
 use Domain\Post\Repository\PostRepository;
-use Domain\Post\Service\PostService;
-use Domain\PostAttachment\Service\PostAttachmentService;
+use Domain\Post\Repository\PostThemeEQRepository;
 
 return [
     'php-di' => [
         PostRepository::class => factory(new DoctrineRepositoryFactory(Post::class)),
+        PostThemeEQRepository::class => factory(new DoctrineRepositoryFactory(PostThemeEQ::class)),
     ]
 ];
