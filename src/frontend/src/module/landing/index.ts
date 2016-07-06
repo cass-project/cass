b0c9aa46f7eb5cfaf3c5848dd841129202952113
dborisenkowork <dborisenkowork@gmail.com>
2016-07-06 23:56:40 +0300

    frontend / public / theme-criteria / shadow, bg-color

--- src/frontend/src/module/landing/index.ts
+++ src/frontend/src/module/landing/index.ts
@@ -1,11 +1,6 @@
 import {Component} from "angular2/core";
-import {PostForm} from "../post/component/index";
-
 @Component({
     template: require('./template.html'),
-    directives: [
-        PostForm
-    ],
 })
 export class LandingComponent
 {
