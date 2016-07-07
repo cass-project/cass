e6ce4c135868bd3685dd3d5fd3983356e52dd25f
hck <art.hacker@ya.ru>
2016-07-07 16:13:40 +0300

    frontend / app / feedback-app / modal / hide & notify on success

--- src/frontend/src/app/feedback-app/src/module/feedback/component/Modals/FeedbackModalComponent/index.ts
+++ src/frontend/src/app/feedback-app/src/module/feedback/component/Modals/FeedbackModalComponent/index.ts
@@ -1,8 +1,12 @@
 import {Component, Input, ViewChild, ElementRef} from "angular2/core";
 
 import {FeedbackService}               from "../../../../../../../../module/feedback/service/FeedbackService";
+import {MessageBusService}             from "../../../../../../../../module/message/service/MessageBusService/index";
+import {MessageBusNotificationsLevel}  from "../../../../../../../../module/message/component/MessageBusNotifications/model";
 import {FeedbackEntity}                from "../../../../../../../../module/feedback/definitions/entity/Feedback";
 import {FeedbackCreateResponseRequest} from "../../../../../../../../module/feedback/definitions/paths/create-response";
+
+declare var jQuery;
 
 @Component({
     selector: 'cass-feedback-modal',
@@ -16,13 +20,14 @@ export class FeedbackModalComponent {
     @ViewChild('feedbackModal') feedbackModal:ElementRef;
     private description:string;
         
-    constructor(private service:FeedbackService){}
+    constructor(private service:FeedbackService, private message:MessageBusService){}
     reply() {
+        jQuery(this.feedbackModal.nativeElement).modal('hide');
         this.service.response(<FeedbackCreateResponseRequest>{
             feedback_id: this.feedback.id,
             description: this.description
         }).subscribe(data => {
-            alert("OK");
+            this.message.replaceLast(MessageBusNotificationsLevel.Success, "Success!");
         })
     }
 
