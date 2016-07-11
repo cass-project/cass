2bcb607d1ac97e77c59bcb0c65d97ad0e9e6f54f
dborisenkowork <dborisenkowork@gmail.com>
2016-07-11 18:58:20 +0300

    cass / vm-db-recreate

--- support-files/vagrant/env/prod/root/usr/bin/vm-db-recreate.sh
+++ support-files/vagrant/env/prod/root/usr/bin/vm-db-recreate.sh
@@ -6,6 +6,5 @@ mysql  -uroot -p"1234" -e "DROP DATABASE cass_testing"
 mysql  -uroot -p"1234" -e "CREATE DATABASE cass_testing"
 
 cd /opt/cass/src/backend
-vendor/bin/phinx migrate -e cass_development
+vendor/bin/phinx migrate -e cass
 vendor/bin/phinx migrate -e cass_testing
-vendor/bin/phinx migrate -e cass_production
