278dc91a5e0ca524796269fdaf0aaeb7a8f67e7f
hck <art.hacker@ya.ru>
2016-07-13 06:13:56 +0300

    frontend / app / profile-im / add service methods

--- src/frontend/src/module/profile-im/service/ProfileIMRESTService.ts
+++ src/frontend/src/module/profile-im/service/ProfileIMRESTService.ts
@@ -4,6 +4,7 @@ import {AbstractRESTService} from "../../common/service/AbstractRESTService";
 import {MessageBusService} from "../../message/service/MessageBusService/index";
 import {AuthToken} from "../../auth/service/AuthToken";
 import {Observable} from "rxjs/Rx";
+import {SendProfileMessageRequest} from "../definitions/paths/send";
 
 @Injectable()
 export class ProfileIMRESTService extends AbstractRESTService
@@ -24,26 +25,34 @@ export class ProfileIMRESTService extends AbstractRESTService
         
         return this.handle(this.http.get(`/backend/api/protected/profile-im/unread`, {headers: authHeader}));
     }
-
-    getMessageFrom(sourceProfileId: number, offset: number, limit: number, markAsRead: boolean)
+    
+    getMessageFrom(sourceProfileId: number, offset: number, limit: number, markAsRead: boolean) : Observable<Response>
     {
+        let authHeader = new Headers();
         let params: URLSearchParams = new URLSearchParams();
+
         params.set('markAsRead', markAsRead.toString());
+        
+        if(this.token.hasToken()){
+            authHeader.append('Authorization', `${this.token.apiKey}`);
+        }
 
         return this.handle(this.http.get(`/backend/api/protected/profile-im/messages/from/${sourceProfileId}/offset/${offset}/limit/${limit}`, {
-            search: params
+            search: params,
+            headers: authHeader
         }));
     }
 
-    sendMessageTo(targetProfileId: number, content: string)
+    sendMessageTo(targetProfileId: number, body:SendProfileMessageRequest) : Observable<Response>
     {
+        
         let authHeader = new Headers();
         if(this.token.hasToken()){
             authHeader.append('Authorization', `${this.token.apiKey}`);
         }
         
-        return this.handle(this.http.put(`/backend/api/protected/profile-im/send/to/${targetProfileId}`, JSON.stringify({
-            content: content
-        }), {headers: authHeader}));
+        return this.handle(this.http.put(
+            `/backend/api/protected/profile-im/send/to/${targetProfileId}`, JSON.stringify(body), {headers: authHeader}
+        ));
     }
 }
--- src/frontend/src/module/profile-im/service/ProfileIMService.ts
+++ src/frontend/src/module/profile-im/service/ProfileIMService.ts
@@ -2,6 +2,9 @@ import {Injectable} from "angular2/core";
 import {ProfileIMRESTService} from "./ProfileIMRESTService";
 import {Observable} from "rxjs/Rx";
 import {UnreadProfileMessagesResponse200} from "../definitions/paths/unread";
+import {SendProfileMessageResponse200, SendProfileMessageRequest} from "../definitions/paths/send";
+import {ProfileIMMessageModel} from "../component/Elements/ProfileIMMessages/model";
+import {ProfileMessagesResponse200} from "../definitions/paths/messages";
 
 @Injectable()
 export class ProfileIMService 
@@ -12,6 +15,35 @@ export class ProfileIMService
     {
         return Observable.create(observer => {
             this.rest.getUnreadMessages().subscribe(
+                data => {
+                    observer.next(data);
+                    observer.complete();
+                },
+                error => {
+                    observer.error(error);
+                }
+            );
+        });
+    }
+    
+    getMessageFrom(sourceProfileId: number, offset: number, limit: number, markAsRead: boolean) : Observable<ProfileMessagesResponse200>
+    {
+        return Observable.create(observer => {
+            this.rest.getMessageFrom(sourceProfileId, offset, limit, markAsRead).subscribe(
+                data => {
+                    observer.next(data);
+                    observer.complete();
+                },
+                error => {
+                    observer.error(error);
+                }
+            );
+        });
+    }
+    sendMessageTo(targetProfileId: number, body:SendProfileMessageRequest) : Observable<SendProfileMessageResponse200>
+    {
+        return Observable.create(observer => {
+            this.rest.sendMessageTo(targetProfileId, body).subscribe(
                 data => {
                     observer.next(data);
                     observer.complete();
