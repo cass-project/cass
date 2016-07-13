bef6ef3868540224ecb0b03068d3af195a830e9e
hck <art.hacker@ya.ru>
2016-07-13 06:16:22 +0300

    frontend / app / profile-im / add ProfileIMTextarea eventEmitter

--- src/frontend/src/module/profile-im/component/Elements/ProfileIMTextarea/index.ts
+++ src/frontend/src/module/profile-im/component/Elements/ProfileIMTextarea/index.ts
@@ -1,5 +1,4 @@
-import {Component, ElementRef, ViewChild} from "angular2/core";
-
+import {Component, ElementRef, ViewChild, Output, EventEmitter} from "angular2/core";
 
 @Component({
     selector: 'cass-profile-im-textarea',
@@ -9,19 +8,21 @@ import {Component, ElementRef, ViewChild} from "angular2/core";
 export class ProfileIMTextarea
 {
     @ViewChild('textarea') textarea:ElementRef;
+    @Output('send') sendEvent = new EventEmitter<string>();
     private hiddenDiv:HTMLDivElement = document.createElement('div');
 
     ngAfterViewInit() {
-        this.hiddenDiv.style.cssText = window.getComputedStyle(this.textarea.nativeElement, null).cssText;
-        this.hiddenDiv.style.width = this.hiddenDiv.style.height = "auto";
-        this.hiddenDiv.style.visibility = "hidden";
-        this.hiddenDiv.style.position = "absolute";
-        this.textarea.nativeElement.parentElement.appendChild(this.hiddenDiv);
-        this.adjust("");
+         this.hiddenDiv.style.cssText = window.getComputedStyle(this.textarea.nativeElement, null).cssText;
+         this.hiddenDiv.style.width = this.hiddenDiv.style.height = "auto";
+         this.hiddenDiv.style.visibility = "hidden";
+         this.hiddenDiv.style.position = "absolute";
+         this.textarea.nativeElement.parentElement.insertBefore(this.hiddenDiv, this.textarea.nativeElement);
+         this.adjust("");
     }
     
-    send(e: Event) {
+    submit(e: Event) {
         e.preventDefault();
+        this.sendEvent.emit(this.textarea.nativeElement.value);
         this.textarea.nativeElement.value = "";
     }
 
