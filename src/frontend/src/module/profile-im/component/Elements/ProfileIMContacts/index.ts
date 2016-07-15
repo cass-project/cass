3901cfda5c0f6380bae73abc7fd20162f3b5d337
hck <art.hacker@ya.ru>
2016-07-15 07:41:14 +0300

    frontend / app / profile-im / init contacts page

--- src/frontend/src/module/profile-im/component/Elements/ProfileIMContacts/index.ts
+++ src/frontend/src/module/profile-im/component/Elements/ProfileIMContacts/index.ts
@@ -1,13 +1,31 @@
 import {Component} from "angular2/core";
+import {ROUTER_DIRECTIVES} from "angular2/router";
+
+import {ContactEntity} from "../../../../contact/definitions/entity/Contact";
+import {ContactService} from "../../../../contact/service/ContactService";
+import {ProfileImage} from "../../../../profile/component/Elements/ProfileImage/index";
 
 @Component({
     selector: 'cass-profile-im-messages',
     template: require('./template.jade'),
     styles: [
         require('./style.shadow.scss')
+    ],
+    directives:[
+        ROUTER_DIRECTIVES,
+        ProfileImage
     ]
 })
 
 export class ProfileIMContacts
 {
+    private contacts:ContactEntity[];
+    
+    constructor(
+        private contactService:ContactService
+    ) {
+        contactService.listContacts().subscribe(
+            data => this.contacts = data.entities
+        );
+    }
 }
