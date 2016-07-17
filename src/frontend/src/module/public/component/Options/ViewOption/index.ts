739148731a1a3f7f24b34dfcf6075ce003ca7aa8
dborisenkowork <dborisenkowork@gmail.com>
2016-07-18 01:42:39 +0300

    frontend / video player option

--- src/frontend/src/module/public/component/Options/ViewOption/index.ts
+++ src/frontend/src/module/public/component/Options/ViewOption/index.ts
@@ -1,4 +1,5 @@
 import {Component, Output, EventEmitter} from "angular2/core";
+import {PublicService} from "../../../service";
 
 @Component({
     selector: 'cass-public-option-view',
@@ -9,4 +10,13 @@ import {Component, Output, EventEmitter} from "angular2/core";
 })
 export class OptionView
 {
+    constructor(private service: PublicService) {}
+
+    isVideoPlayerAvailable(): boolean {
+        if(this.service.criteria.has('content_type')) {
+            return this.service.criteria.getByCode('content_type').params.type === 'video';
+        }else{
+            return false;
+        }
+    }
 }
