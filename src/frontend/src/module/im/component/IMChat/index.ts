4ca37e7892620b931c6ed0e4ff84f7a5e3c864b6
hck <art.hacker@ya.ru>
2016-07-19 08:01:35 +0300

    frontend / im / add IMChat component

--- src/frontend/src/module/im/component/IMChat/index.ts
+++ src/frontend/src/module/im/component/IMChat/index.ts
@@ -1,19 +1,19 @@
 import {Component, Input} from "angular2/core";
 import {DateFormatter} from 'angular2/src/facade/intl';
 
-import {ProfileMessageExtendedEntity} from "../../../definitions/entity/ProfileMessage";
+import {IMMessageExtendedEntity} from "../../definitions/entity/IMMessage";
 
 @Component({
-    selector: 'cass-profile-im-chat-history',
+    selector: 'cass-im-chat',
     template: require('./template.jade'),
     styles: [
         require('./style.shadow.scss')
     ]
 })
 
-export class ProfileIMChatHistory
+export class IMChat
 {
-    @Input('history') history:ProfileMessageExtendedEntity[];
+    @Input('messages') messages:IMMessageExtendedEntity[];
 
     dateFormat(date:string, format:string) {
         return DateFormatter.format(new Date(date), 'pt', format);
