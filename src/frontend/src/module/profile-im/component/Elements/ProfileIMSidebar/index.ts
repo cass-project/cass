a08fa5e01090ea1e1cb63b3ac1e9ec1e731f500d
hck <art.hacker@ya.ru>
2016-07-21 12:38:21 +0300

    frontend / profile-im / add contacts in sidebar

--- src/frontend/src/module/profile-im/component/Elements/ProfileIMSidebar/index.ts
+++ src/frontend/src/module/profile-im/component/Elements/ProfileIMSidebar/index.ts
@@ -1,13 +1,39 @@
 import {Component} from "angular2/core";
+import {ROUTER_DIRECTIVES} from "angular2/router";
+
+import {ProfileImage} from "../../../../profile/component/Elements/ProfileImage/index";
+import {ContactEntity} from "../../../../contact/definitions/entity/Contact";
+import {ContactService} from "../../../../contact/service/ContactService";
 
 @Component({
     selector: 'cass-profile-im-sidebar',
     template: require('./template.jade'),
     styles: [
         require('./style.shadow.scss')
+    ],
+    directives:[
+        ROUTER_DIRECTIVES,
+        ProfileImage
     ]
 })
 
 export class ProfileIMSidebar
 {
+    private isVisible:boolean;
+    private isLoading = true;
+    private contacts:ContactEntity[];
+
+    constructor(private contactService:ContactService) {
+        contactService.listContacts().subscribe(
+            
+            data => {
+                this.contacts = data.entities;
+                this.isLoading = false;
+            }
+        );
+    }
+    
+    toggleState() {
+        this.isVisible = !this.isVisible;
+    }
 }
