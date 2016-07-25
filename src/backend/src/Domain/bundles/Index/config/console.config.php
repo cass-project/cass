d1faa465219b9b628dff65b41d46031a2c776529
dborisenkowork <dborisenkowork@gmail.com>
2016-07-25 04:06:44 +0300

    backend / rm index command

--- src/backend/src/Domain/bundles/Index/config/console.config.php
+++ src/backend/src/Domain/bundles/Index/config/console.config.php
@@ -1,15 +1,10 @@
 <?php
 namespace Domain\Fake;
 
-use Domain\Index\Scripts\UpdateIndexScript\UpdateIndexScript;
-
 return [
     'php-di' => [
         'config.console' => [
             'commands' => [
-                'index' => [
-                    UpdateIndexScript::class
-                ],
             ],
         ],
     ],
