977876903e515a8fe3ee8437a0096d7daf3e6d01
hck <art.hacker@ya.ru>
2016-07-26 13:28:16 +0300

    frontend / vm-init / bugfix when install mongo, remove magic sevenshebang

--- support-files/vagrant/env/stage/root/usr/bin/vm-init.sh
+++ support-files/vagrant/env/stage/root/usr/bin/vm-init.sh
@@ -1,4 +1,4 @@
-7#!/usr/bin/env bash
+#!/usr/bin/env bash
 
 # Locale issues
 sudo apt-get install language-pack-en language-pack-en-base -y
@@ -118,5 +118,5 @@ sudo service nginx restart
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
-sudo apt-get install mongodb-org
+sudo apt-get install -y mongodb-org
 sudo service mongodb restart
