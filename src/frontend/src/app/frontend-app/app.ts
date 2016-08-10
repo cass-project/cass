6476413d8affd16e9f499abd9cb5f5eb4cb9dd49
dborisenko <dborisenkowork@gmail.com>
2016-08-10 15:05:09 +0300

    frontend / really, fk off sce

--- src/frontend/src/app/frontend-app/app.ts
+++ src/frontend/src/app/frontend-app/app.ts
@@ -16,12 +16,21 @@ import {bootstrap} from "@angular/platform-browser-dynamic";
 import {enableProdMode} from '@angular/core';
 import {HTTP_PROVIDERS} from "@angular/http";
 import {ROUTER_PROVIDERS} from "@angular/router-deprecated";
+import {Injectable, provide} from '@angular/core';
+import {DomSanitizationService, SecurityContext} from '@angular/platform-browser';
 
 import {App} from "./index";
 import {FrontlineService, frontline} from "../../module/frontline/service";
 import {AuthToken} from "../../module/auth/service/AuthToken";
 
 enableProdMode();
+
+@Injectable()
+export class NoSanitizationService {
+    sanitize(ctx: SecurityContext, value: any): string {
+        return value;
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     frontline(session => {
@@ -32,6 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 provide(FrontlineService, {useValue: frontline}),
                 ROUTER_PROVIDERS,
                 HTTP_PROVIDERS,
+                provide(DomSanitizationService, { useClass: NoSanitizationService }),
                 provide(Window, {useValue: session}),
                 provide(AuthToken, {useFactory: () => {
                     let token = new AuthToken();
