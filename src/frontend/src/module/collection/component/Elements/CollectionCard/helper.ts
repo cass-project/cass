d8990864468649a2249eb4bad70c3e7498784f3e
dborisenkowork <dborisenkowork@gmail.com>
2016-10-10 21:55:41 +0300

    frontend / collection card / complete

--- src/frontend/src/module/collection/component/Elements/CollectionCard/helper.ts
+++ src/frontend/src/module/collection/component/Elements/CollectionCard/helper.ts
@@ -3,6 +3,7 @@ import {CollectionEntity} from "../../../definitions/entity/collection";
 import {QueryTarget, queryImage} from "../../../../avatar/functions/query";
 import {ThemeService} from "../../../../theme/service/ThemeService";
 import {CollectionService} from "../../../service/CollectionService";
+import {Theme} from "../../../../theme/definitions/entity/Theme";
 
 @Injectable()
 export class CollectionCardHelper
@@ -15,18 +16,31 @@ export class CollectionCardHelper
     ) {}
 
     setCollection(collection: CollectionEntity) {
-        console.log('ng changes', collection);
         this.collection = collection;
     }
 
-    getImageURL(): string {
-        let image = queryImage(QueryTarget.Card, this.collection.image).public_path;
+    getCollectionName(): string {
+        return this.collection.title;
+    }
 
-        return `url('${image}')`;
+    getImageURL(): string {
+        return queryImage(QueryTarget.Card, this.collection.image).public_path;
     }
 
     hasTheme(): boolean {
         return this.collection.theme_ids.length > 0;
+    }
+
+    getThemes(): Theme[] {
+        let result = [];
+
+        this.collection.theme_ids.forEach(id => {
+            if(this.theme.hasWithId(id)) {
+                result.push(this.theme.findById(id));
+            }
+        });
+
+        return result;
     }
 
     getThemeTitle(): string {
--- src/frontend/src/module/collection/service/CollectionService.ts
+++ src/frontend/src/module/collection/service/CollectionService.ts
@@ -10,9 +10,9 @@ export class CollectionService
 
     getRouterParams(collection: CollectionEntity) {
         if(collection.owner.type === 'profile') {
-            return ['/profile', collection.owner.id, 'collections', collection.sid];
+            return ['/', 'profile', collection.owner.id, 'collections', collection.sid];
         }else if(collection.owner.type === 'community') {
-            return ['/community', collection.owner.id, 'collections', collection.sid];
+            return ['/', 'community', collection.owner.id, 'collections', collection.sid];
         }else{
             throw new Error(`Unknown owner type for collection "${collection.sid}"`);
         }
