c58b3937b543198648a9f13be51b251cb9c58491
hck <2111072@gmail.com>
2016-10-17 00:00:00 +0300

    frontend / TextParser / add Markdown themes. Add URL white list.

--- src/frontend/src/module/common/component/TextParser/index.ts
+++ src/frontend/src/module/common/component/TextParser/index.ts
@@ -4,18 +4,47 @@ var md = require('markdown-it')({
     quotes: ">"
 });
 
-import {Component, Input} from "@angular/core";
+import {Component, Input, ViewEncapsulation} from "@angular/core";
 
 @Component({
     selector: 'cass-text-parser',
+    styles: [
+        require('./style.shadow.scss')
+    ],
+    encapsulation: ViewEncapsulation.None,
     template: require('./template.jade')
 })
 
 export class TextParser
 {
     @Input('text') text: string;
+    private whiteList = [
+        "p\/home.*"  
+    ];
 
-    parseText(){
+    parseText() {
+        md.validateLink = (link) => { return this.validateLink(link) };
         return md.render(this.text);
+    }
+    
+    isInternalLink(link): boolean {
+        return new RegExp("^http:\/\/"+window.location.host+"\/.*$").test(link);
+    }
+    
+    isInWhiteList(link) : boolean {
+        for(let whiteLink of this.whiteList) {
+            let whiteLinkRegExp:RegExp = new RegExp("^http:\/\/"+window.location.host+"\/" + whiteLink + "$");
+            if(whiteLinkRegExp.test(link)) {
+                return true;
+            }
+        }
+        return false;   
+    }
+    
+    validateLink(link) : boolean {
+        if(this.isInternalLink(link)) {
+            return this.isInWhiteList(link) 
+        }
+        return true;
     }
 }
